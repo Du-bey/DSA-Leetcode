18614250ddcac0bff30dfc996b58f7731c1fe8e0
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-02-10 14:32:47 +0530

    Time: 1 ms (81.42%) | Memory: 44.8 MB (5.63%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -2,21 +2,23 @@ class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
         boolean[] vis = new boolean[n];
-        dfs(0, rooms, vis);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        vis[0] = true;
+        while(!q.isEmpty()){
+            int node = q.poll();
+            for(int it : rooms.get(node)){
+                if(!vis[it]){
+                    q.add(it);
+                    vis[it] = true;
+                }
+            }
+        }
         for(int i =0;i<n;i++){
             if(!vis[i]) {
                 return false;
             }
         }
         return true;
-    }
-
-    public void dfs(int node, List<List<Integer>> rooms, boolean[] vis) {
-        vis[node] = true;
-        for(int it : rooms.get(node)){
-            if(!vis[it]){
-                dfs(it, rooms, vis);
-            }
-        }
     }
 }
