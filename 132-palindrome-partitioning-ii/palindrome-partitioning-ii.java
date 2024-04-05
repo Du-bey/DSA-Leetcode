920379b4c9074f500d03c00df452a1482344fcfd
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-04-05 17:45:14 +0530

    Time: 2524 ms (21.39%) | Memory: 41 MB (95.70%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,22 +1,19 @@
 class Solution {
     public int minCut(String s) {
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return f(s, n, dp, 0) - 1;
-    }
-    public int f(String s, int n, int[] dp, int i){
-        if(i == n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int minCost = Integer.MAX_VALUE;
-        for(int j = i;j<n;j++){
-            if(isPalindrome(i, j, s)){
-                int cost = 1 + f(s, n, dp, j+1);
-                minCost = Math.min(minCost, cost);
+        int[] dp = new int[n+1];
+        
+        for(int i = n-1;i>=0;i--){
+            int minCost = Integer.MAX_VALUE;
+            for(int j = i;j<n;j++){
+                if(isPalindrome(i, j, s)){
+                    int cost = 1 + dp[j+1];
+                    minCost = Math.min(minCost, cost);
+                }
             }
+            dp[i] = minCost;
         }
-        dp[i] = minCost;
-        return dp[i];
+        return dp[0] - 1;
     }
 
     public boolean isPalindrome(int i, int j, String s){
