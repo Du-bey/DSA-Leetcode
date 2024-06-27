2460880622ad6fb898aaea686e22c74c147de87f
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-06-27 18:35:16 +0530

    Time: 2 ms (73.91%) | Memory: 42.4 MB (31.63%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,44 +1,44 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<Triplet> q = new LinkedList<>();
         int n = grid.length;
         int m = grid[0].length;
-
         boolean[][] vis = new boolean[n][m];
+        Queue<Triplet> q = new LinkedList<>();
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(grid[i][j] == 2){
-                    q.add(new Triplet(0,i,j));
+                    q.add(new Triplet(i,j,0));
                     vis[i][j] = true;
                 }
             }
         }
         int ans = 0;
         while(!q.isEmpty()){
-            int time = q.peek().first;
-            int r = q.peek().second;
-            int c = q.peek().third;
+            Triplet trip = q.peek();
+            int r= trip.r;
+            int c = trip.c;
+            int t = trip.tim;
             q.remove();
-
-            ans = Math.max(ans, time);
-
-            int[] delr = {0, -1, 0, 1};
-            int[] delc = {-1, 0, 1, 0};
+            ans = Math.max(ans, t);
+            int[] delr = {-1, 0, 1, 0};
+            int[] delc = {0, 1, 0, -1};
 
             for(int i =0;i<4;i++){
                 int newr = r + delr[i];
                 int newc = c + delc[i];
                 if(newr >=0 && newc >= 0 && newr < n && newc < m 
                     && grid[newr][newc] == 1 && !vis[newr][newc]){
-                        q.add(new Triplet(time +1, newr, newc));
+                        q.add(new Triplet(newr, newc, t +1));
                         vis[newr][newc] = true;
+                        //ans = t;
                 }
             }
-
         }
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == 1 && !vis[i][j]) return -1;
+                if(!vis[i][j] && grid[i][j] == 1){
+                    return -1;
+                }
             }
         }
         return ans;
@@ -46,14 +46,12 @@ class Solution {
 }
 
 public class Triplet{
-    int first;
-    int second;
-    int third;
-
-
-    public Triplet(int first, int second, int third){
-        this.first = first;
-        this.second = second;
-        this.third = third;
+    int r;
+    int c;
+    int tim;
+    public Triplet(int r, int c, int tim){
+        this.r = r;
+        this.c = c;
+        this.tim = tim;
     }
 }
