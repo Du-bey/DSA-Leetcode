c2ba72cb221a91e72a54197d892af64fb07460f9
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-07-14 21:29:11 +0530

    Time: 2 ms (74.03%) | Memory: 42.8 MB (12.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,42 +1,39 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-        Queue<Triplet> q = new LinkedList<>();
+    public int orangesRotting(int[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        boolean [][] vis = new boolean[n][m];
+        Queue<Pair> q = new LinkedList<>();
         for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j] == 2){
-                    q.add(new Triplet(i,j,0));
+            for(int j = 0;j<m;j++){
+                if(board[i][j] == 2){
+                    q.add(new Pair(i, j, 0));
                     vis[i][j] = true;
                 }
             }
         }
         int ans = 0;
+        int[] delr = {0, 1, 0, -1};
+        int[] delc = {1, 0, -1, 0};
         while(!q.isEmpty()){
-            Triplet trip = q.peek();
-            int r= trip.r;
-            int c = trip.c;
-            int t = trip.tim;
+            Pair p = q.peek();
             q.remove();
+            int r = p.r;
+            int c = p.c;
+            int t = p.t;
             ans = Math.max(ans, t);
-            int[] delr = {-1, 0, 1, 0};
-            int[] delc = {0, 1, 0, -1};
-
             for(int i =0;i<4;i++){
                 int newr = r + delr[i];
                 int newc = c + delc[i];
-                if(newr >=0 && newc >= 0 && newr < n && newc < m 
-                    && grid[newr][newc] == 1 && !vis[newr][newc]){
-                        q.add(new Triplet(newr, newc, t +1));
-                        vis[newr][newc] = true;
-                        //ans = t;
+                if(newr >=0 && newc >=0 && newr < n && newc < m && !vis[newr][newc] && board[newr][newc] == 1){
+                    vis[newr][newc] = true;
+                    q.add(new Pair(newr, newc, t +1));  
                 }
             }
         }
         for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] == 1){
+            for(int j = 0;j<m;j++){
+                if(!vis[i][j] && board[i][j] == 1){
                     return -1;
                 }
             }
@@ -45,13 +42,13 @@ class Solution {
     }
 }
 
-public class Triplet{
+class Pair{
     int r;
     int c;
-    int tim;
-    public Triplet(int r, int c, int tim){
+    int t;
+    public Pair(int r, int c, int t){
         this.r = r;
         this.c = c;
-        this.tim = tim;
+        this.t = t;
     }
 }
