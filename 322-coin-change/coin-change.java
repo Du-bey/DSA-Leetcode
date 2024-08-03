c5978e01517b262b276f5a7f43f1e6b81f1ea8f7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-03 15:53:33 +0530

    Time: 140 ms (7.07%) | Memory: 45.1 MB (23.49%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,21 +1,23 @@
 class Solution {
-    public int coinChange(int[] coins, int target) {
+    int mod = 100000000;
+    public int coinChange(int[] coins, int t) {
         int n = coins.length;
-        int[][] dp = new int[n][target+1];
-        for(int t =0;t <= target;t++){
-            if(t%coins[0] == 0) dp[0][t] = t/coins[0];
-            else dp[0][t] = 10000000;
+        int[][] dp = new int[n][t+1];
+
+        for(int i =0;i<=t;i++){
+            dp[0][i] = (i%coins[0]) == 0 ? i/coins[0] : mod;
         }
+
         for(int i =1;i<n;i++){
-            for(int j =0;j<= target;j++){
+            for(int j = 0;j<=t;j++){
                 int notTake = dp[i-1][j];
-                int take = 10000000;
+                int take = mod;
                 if(coins[i] <= j){
                     take = 1 + dp[i][j-coins[i]];
                 }
                 dp[i][j] = Math.min(take, notTake);
             }
         }
-        return dp[n-1][target] >= 10000000 ? -1 : dp[n-1][target];
+        return dp[n-1][t] >= mod ? -1 : dp[n-1][t];
     }
 }
