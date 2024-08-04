f5ad351347d8582e78a5ff0c3e9638a4ab88dbb8
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-05 00:22:13 +0530

    Time: 3 ms (64.07%) | Memory: 44.1 MB (40.77%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,18 +2,20 @@ class Solution {
     public int minimumTotal(List<List<Integer>> t) {
         int n = t.size();
         if(n == 1) return t.get(0).get(0);
-        int[][] dp = new int[n][n];
+        int[] prev = new int[n];
         for(int i =0;i<n;i++){
-            dp[n-1][i] = t.get(n-1).get(i);
+            prev[i] = t.get(n-1).get(i);
         }
         for(int i = n-2;i>=0;i--){
+            int[] cur = new int[n];
             for(int j=0;j<=i;j++){
-                int down = dp[i+1][j];
-                int right = dp[i+1][j+1];
-                dp[i][j] = t.get(i).get(j) + Math.min(down, right);
+                int down = prev[j];
+                int right = prev[j+1];
+                cur[j] = t.get(i).get(j) + Math.min(down, right);
             }
+            prev = cur;
         }
-        return dp[0][0];
+        return prev[0];
     }
 
     public int f(int i, int j, List<List<Integer>> t, int n, int[][] dp){
