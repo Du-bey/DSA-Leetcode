4950ff1b9030477ce058039e6fa47c6bc03c22e7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-05 00:52:43 +0530

    Time: 8 ms (36.28%) | Memory: 45.1 MB (60.08%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,24 +1,40 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
-        int[][] dp = new int[n][n];
-        for(int i =0;i<n;i++){
+        int m = matrix[0].length;
+        int ans = Integer.MAX_VALUE;
+        int [][] dp = new int[n][m];
+        
+        for(int i =0;i<m;i++){
             dp[0][i] = matrix[0][i];
         }
+
         for(int i =1;i<n;i++){
-            for(int j =0;j<n;j++){
-                int up = dp[i-1][j];
-                int ld = Integer.MAX_VALUE;
-                int rd = Integer.MAX_VALUE;
-                if(j >0) ld = dp[i-1][j-1];
-                if(j<n-1) rd = dp[i-1][j+1];
-                dp[i][j] = matrix[i][j] + Math.min(up, Math.min(ld, rd));
+            for(int j =0;j<m;j++){
+                int lu = 10000000;
+                int ru = 10000000;
+                if(j-1 >= 0) lu = dp[i-1][j-1];
+                int u = dp[i-1][j];
+                if(j < m-1) ru = dp[i-1][j+1];
+                dp[i][j] = matrix[i][j] + Math.min(lu, Math.min(u, ru));
             }
         }
-        int ans = dp[n-1][0];
-        for(int i =1;i<n;i++){
+
+        for(int i = 0;i<m;i++){
             ans = Math.min(ans, dp[n-1][i]);
         }
         return ans;
+    }
+
+    public int f(int i, int j, int[][] matrix, int n, int m, int [][] dp){
+        if( j<0 || j > m-1) return 10000000;
+        if(i == 0) return matrix[0][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int lu = f(i-1, j-1, matrix, n, m, dp);
+        int u = f(i-1, j, matrix, n, m, dp);
+        int ru = f(i-1, j+1, matrix, n, m, dp);
+
+        dp[i][j] = matrix[i][j] + Math.min(lu, Math.min(u, ru));
+        return dp[i][j];
     }
 }
