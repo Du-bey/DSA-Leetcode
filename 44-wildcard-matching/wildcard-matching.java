78bfa0a7db9835600fda0244187290edf99ab484
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-06 02:01:18 +0530

    Time: 19 ms (67.04%) | Memory: 48.4 MB (23.09%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -6,25 +6,25 @@ class Solution {
         for(int[] row: dp){
             Arrays.fill(row, -1);
         }
-        return f(n-1, m-1, s, p, dp);
+        return f(n, m, s, p, dp);
     }
 
     public boolean f(int i, int j, String s, String p, int[][] dp){
-        if(i < 0 && j <0) return true;
-        if(i >= 0 && j <0) return false;
-        if(i < 0 && j >= 0){
-            while(j >= 0){
-                if(p.charAt(j) != '*') return false;
+        if(i == 0 && j == 0) return true;
+        if(i > 0 && j == 0) return false;
+        if(i == 0 && j > 0){
+            while(j > 0){
+                if(p.charAt(j-1) != '*') return false;
                 j--;
             }
             return true;
         }
         if(dp[i][j] != -1) return dp[i][j] == 0 ? false : true;
 
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+        if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
             dp[i][j] = f(i-1, j-1, s, p, dp) == true ? 1 : 0;
         }
-        else if(p.charAt(j) == '*'){
+        else if(p.charAt(j-1) == '*'){
             dp[i][j] = (f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp)) == true ? 1 : 0;
         }
         else{
