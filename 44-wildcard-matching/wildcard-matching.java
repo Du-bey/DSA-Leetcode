cf5b3aa0fb39b533cf9e91a767770a170420ab53
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-06 01:59:53 +0530

    Time: 19 ms (67.04%) | Memory: 48.5 MB (23.09%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+        return f(n-1, m-1, s, p, dp);
+    }
+
+    public boolean f(int i, int j, String s, String p, int[][] dp){
+        if(i < 0 && j <0) return true;
+        if(i >= 0 && j <0) return false;
+        if(i < 0 && j >= 0){
+            while(j >= 0){
+                if(p.charAt(j) != '*') return false;
+                j--;
+            }
+            return true;
+        }
+        if(dp[i][j] != -1) return dp[i][j] == 0 ? false : true;
+
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+            dp[i][j] = f(i-1, j-1, s, p, dp) == true ? 1 : 0;
+        }
+        else if(p.charAt(j) == '*'){
+            dp[i][j] = (f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp)) == true ? 1 : 0;
+        }
+        else{
+            dp[i][j] = 0;
+        }
+        return dp[i][j] == 0 ? false : true;
+    }
+}
