71457c768dcdfc00c238c6400b848bda48237245
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-07 13:58:35 +0530

    Time: 68 ms (28.27%) | Memory: 78.8 MB (9.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,21 +1,17 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        int[][] dp = new int[n+1][n+1];
+        for(int i = n-1;i>=0;i--){
+            for(int prev =i-1;prev>=-1;prev--){
+                int len = 0 + dp[i+1][prev+1];
+                if(prev == -1 || nums[i] > nums[prev]) {
+                    len = Math.max(len, 1 + dp[i+1][i+1]);
+                }
+                dp[i][prev+1] = len;
+            }
         }
-        return f(0, -1, nums, dp);
+        return dp[0][0];
     }
 
-    public int f(int i, int prev, int[] nums, int[][] dp){
-        if(i == nums.length) return 0;
-        
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
-        int notTake = 0 + f(i+1, prev, nums, dp);
-        int take = -100000000;
-        if(prev == -1 || nums[i] > nums[prev]) take = 1 + f(i+1, i, nums, dp);
-        dp[i][prev+1] = Math.max(take, notTake);
-        return Math.max(take, notTake);
-    }
 }
