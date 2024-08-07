c85ce950b02da8b048df84c264c016ddf7d35cf8
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-07 15:09:00 +0530

    Time: 14 ms (60.19%) | Memory: 42.7 MB (93.76%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -5,30 +5,27 @@ class Solution {
         int n = nums.length;
         int[] dp = new int[n];
         int[] hash = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        int ind = 0;
         for(int i =0;i<n;i++){
-            dp[i] = 1;
             hash[i] = i;
-        }
-        int max = 0;
-        for(int i =0;i<n;i++){
-            for(int prev = 0;prev < i;prev++){
-                if(nums[i] % nums[prev] == 0 && (1 + dp[prev]) > dp[i]){
-                    dp[i] = 1 + dp[prev];
-                    hash[i] = prev;
-                    if(dp[i] > dp[max]){
-                        max = i;
-                    }
-                    
+            for(int j =0;j<i;j++){
+                if(nums[i] % nums[j] == 0 && 1 + dp[j] > dp[i]){
+                    dp[i] = 1 + dp[j];
+                    hash[i] = j;
                 }
             }
+            if(dp[i] > max){
+                ind = i;
+                max = dp[i];
+            }
         }
-        while(hash[max] != max){
-            ans.add(nums[max]);
-            max = hash[max];
+        ans.add(nums[ind]);
+        while(hash[ind] != ind){
+            ind = hash[ind];
+            ans.add(nums[ind]);
         }
-        ans.add(nums[max]);
         return ans;
     }
 }
-// [1,2,4,8]
-// [0,1,2,3]
