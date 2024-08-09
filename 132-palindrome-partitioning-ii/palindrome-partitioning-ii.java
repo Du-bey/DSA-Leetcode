b38121722a4dfea51c71ce7e9e946a2f7363f846
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-10 00:21:16 +0530

    Time: 2321 ms (46.94%) | Memory: 41.8 MB (54.50%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,22 +1,27 @@
 class Solution {
     public int minCut(String s) {
         int n = s.length();
-        int[] dp = new int[n+1];
-        
-        for(int i = n-1;i>=0;i--){
-            int minCost = Integer.MAX_VALUE;
-            for(int j = i;j<n;j++){
-                if(isPalindrome(i, j, s)){
-                    int cost = 1 + dp[j+1];
-                    minCost = Math.min(minCost, cost);
-                }
-            }
-            dp[i] = minCost;
-        }
-        return dp[0] - 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(0, s, n, dp) - 1;
     }
 
-    public boolean isPalindrome(int i, int j, String s){
+    public int f(int i, String s, int n, int[] dp){
+        if(i == n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+        int min = Integer.MAX_VALUE;
+        for(int j =i;j<n;j++){
+            if(isPalindrome(i, s, j)){
+                int cost = 1 + f(j+1, s, n, dp);
+                min = Math.min(min, cost);
+            }
+        }
+        dp[i] = min;
+        return dp[i];
+    }
+
+    public boolean isPalindrome(int i, String s, int j){
         while(i < j){
             if(s.charAt(i) != s.charAt(j)) return false;
             i++;
