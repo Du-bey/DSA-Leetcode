b88ec767e48111fb5464e57d22f7d49ffe0f88c6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-10 22:42:40 +0530

    Time: 1 ms (99.62%) | Memory: 44.3 MB (33.72%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,29 +2,20 @@ class Solution {
     public int minimumTotal(List<List<Integer>> t) {
         int n = t.size();
         if(n == 1) return t.get(0).get(0);
-        int[] prev = new int[n];
-        for(int i =0;i<n;i++){
-            prev[i] = t.get(n-1).get(i);
+        int [][] dp = new int[n][n];
+        for(int [] row : dp){
+            Arrays.fill(row, -1);
         }
-        for(int i = n-2;i>=0;i--){
-            int[] cur = new int[n];
-            for(int j=0;j<=i;j++){
-                int down = prev[j];
-                int right = prev[j+1];
-                cur[j] = t.get(i).get(j) + Math.min(down, right);
-            }
-            prev = cur;
-        }
-        return prev[0];
+        return f(0, 0, t, n, dp);
     }
+    public int f(int i, int j, List<List<Integer>> t, int n, int [][] dp){
+        if(i == n) return 0;
 
-    public int f(int i, int j, List<List<Integer>> t, int n, int[][] dp){
-        if(i == n-1) return t.get(i).get(j);
         if(dp[i][j] != -1) return dp[i][j];
-        int down = f(i+1, j, t, n, dp);
-        int right = f(i+1, j+1, t, n, dp);
-        dp[i][j] = t.get(i).get(j) + Math.min(down, right);
+        int dr = f(i+1, j+1, t, n, dp);
+        int d = f(i+1, j, t, n, dp);
+        int cost = t.get(i).get(j) + Math.min(dr, d);
+        dp[i][j] = cost;
         return dp[i][j];
-        
     }
 }
