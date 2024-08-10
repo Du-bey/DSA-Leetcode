6e8d0f743a3fc2a66e9770283c481092745d6ff2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-11 00:37:14 +0530

    Time: 45 ms (52.77%) | Memory: 46.9 MB (47.57%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -2,22 +2,29 @@ class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
-        for(int i =0;i<n;i++){
-            sum+= nums[i];
+        for(int no : nums){
+            sum += no;
         }
         if(sum % 2 == 1) return false;
-        sum = sum/2;
-        boolean[][] dp = new boolean[n][sum+1];
-        for(int i =0;i<n;i++) dp[i][0] = true;
-        if(nums[0] <= sum) dp[0][nums[0]] = true;
-        for(int i= 1;i<n;i++){
-            for(int t =1;t < sum+1;t++){
-                boolean notTake = dp[i-1][t];
+        return isSubsetSum(n, nums, sum/2);
+    }
+
+    static Boolean isSubsetSum(int n, int arr[], int s){
+        // code here
+        boolean[][] dp = new boolean[n][s+1];
+        for(int i = 0;i<n;i++){
+            dp[i][0] = true;
+        }
+        
+        if(arr[0] <= s) dp[0][arr[0]] = true;
+        for(int i =1;i<n;i++){
+            for(int sum = 1;sum<=s;sum++){
                 boolean take = false;
-                if(nums[i] <= t) take = dp[i-1][t-nums[i]];
-                dp[i][t] = take || notTake;
+                boolean notTake = dp[i-1][sum];
+                if(sum >= arr[i]) take = dp[i-1][sum - arr[i]];
+                dp[i][sum] = take || notTake;
             }
         }
-        return dp[n-1][sum];
+        return dp[n-1][s];
     }
 }
