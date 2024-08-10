c2e57edcecdda916fd243dae4e0564d886a7c7d1
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-10 18:54:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (29.54%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return f(n-1, nums, dp);
+    }
+
+    public int f(int i, int[] nums, int [] dp){
+        if(i < 0) return 0;
+        if(i == 0) return nums[0];
+
+        if(dp[i] != -1) return dp[i];
+
+        int take = nums[i] + f(i-2, nums, dp);
+        int notTake = 0 + f(i-1, nums, dp);
+        dp[i] = Math.max(take, notTake);
+        return dp[i];
+    }
+}
