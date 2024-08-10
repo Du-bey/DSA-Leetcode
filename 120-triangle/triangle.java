f1d25d8e9b96adc4c4e8ceeeabaf813a8bea5ca5
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-10 22:46:45 +0530

    Time: 3 ms (64.06%) | Memory: 44 MB (53.35%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,20 +2,15 @@ class Solution {
     public int minimumTotal(List<List<Integer>> t) {
         int n = t.size();
         if(n == 1) return t.get(0).get(0);
-        int [][] dp = new int[n][n];
-        for(int [] row : dp){
-            Arrays.fill(row, -1);
+        int [][] dp = new int[n+1][n+1];
+        for(int i =n-1;i>=0;i--){
+            for(int j =0;j<=i;j++){
+                int dr = dp[i+1][j+1];
+                int d = dp[i+1][j];
+                int cost = t.get(i).get(j) + Math.min(dr, d);
+                dp[i][j] = cost;
+            }
         }
-        return f(0, 0, t, n, dp);
-    }
-    public int f(int i, int j, List<List<Integer>> t, int n, int [][] dp){
-        if(i == n) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        int dr = f(i+1, j+1, t, n, dp);
-        int d = f(i+1, j, t, n, dp);
-        int cost = t.get(i).get(j) + Math.min(dr, d);
-        dp[i][j] = cost;
-        return dp[i][j];
+        return dp[0][0];
     }
 }
