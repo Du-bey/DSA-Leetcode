267acc5b76b6b68cb3f862afe750e29f898c9127
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-14 16:40:06 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (79.10%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,16 +1,43 @@
 class Solution {
     public boolean isValidSudoku(char[][] b) {
-        Set seen = new HashSet();
-        for(int i =0;i<9;i++){
-            for(int j =0;j<9;j++){
+        int[] hash = new int[9];
+        for(int i =0;i <9;i++){
+            for(int j=0;j<9;j++){
                 char c = b[i][j];
                 if(c != '.'){
-                    if(!seen.add(c + " in row " + i) ||
-                       !seen.add(c + " in column " + j) ||
-                       !seen.add(c + " in block " + i/3 + "-" + j/3))
-                       return false;
+                    if(hash[c - '1'] != 0) return false;
+                    hash[c - '1']++;
                 }
             }
+            Arrays.fill(hash, 0);
+        }
+        for(int j=0;j<9;j++){
+            for(int i =0;i <9;i++){
+                char c = b[i][j];
+                if(c != '.'){
+                    if(hash[c - '1'] != 0) return false;
+                    hash[c - '1']++;
+                }
+            }
+            Arrays.fill(hash, 0);
+        }
+
+        for(int i =0;i <9;i+= 3){
+            for(int j=0;j<9;j+=3){
+                for(int k = i;k<i+3;k++){
+                    for(int l = j;l<j+3;l++){
+                        char c = b[k][l];
+                        if(c != '.'){
+                            if(hash[c - '1'] != 0) {
+                                return false;
+                            }
+                            hash[c - '1']++;
+                        }
+                    }
+                }
+                Arrays.fill(hash, 0);
+            }
+            
         }
         return true;
     }
