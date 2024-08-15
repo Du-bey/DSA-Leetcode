e41334ffb3c35147bce2cf96b0f23825b530a8e6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-15 17:45:58 +0530

    Time: 25 ms (51.36%) | Memory: 44.8 MB (75.35%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,36 +1,30 @@
 class Solution {
-
-
-    public static int findMax(int[] v) {
-        int maxi = Integer.MIN_VALUE;;
-        int n = v.length;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, v[i]);
+    public int minEatingSpeed(int[] piles, int t) {
+        int n = piles.length;
+        int l = 1;
+        int h = 0;
+        for(int no : piles){
+            h = Math.max(h, no);
         }
-        return maxi;
+        while(l<=h){
+            int mid = (l+h)/2;
+            int hours = countHour(piles, mid);
+            if(hours > t){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
+            }
+        }
+        return l;
     }
 
-    public static int calculateTotalHours(int[] v, int hourly) {
+    public static int countHour(int[] v, int hourly) {
         int totalH = 0;
         int n = v.length;
         for (int i = 0; i < n; i++) {
             totalH += Math.ceil((double)(v[i]) / (double)(hourly));
         }
         return totalH;
-    }
-
-
-    public int minEatingSpeed(int[] v, int h) {
-        int low = 1, high = findMax(v);
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int totalH = calculateTotalHours(v, mid);
-            if (totalH <= h) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
     }
 }
