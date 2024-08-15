0f7d46f7809b467a799511a9e23737bad14d2b9f
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-15 16:46:10 +0530

    Time: 82 ms (55.39%) | Memory: 58 MB (48.73%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        if (n == 1) return 1;
+        Stack<Double> st = new Stack<>();
+        int[][] combine = new int[position.length][2];
+        for (int i = 0; i < n; i++) {
+            combine[i][0] = position[i];
+            combine[i][1] = speed[i];
+        }
+
+        Arrays.sort(combine, (r1, r2) -> Integer.compare(r1[0] , r2[0]));
+        for(int i = n-1; i>=0;i--){
+            double time = (double) (target - combine[i][0]) / combine[i][1];
+            if(!st.isEmpty() && time <= st.peek()){
+                continue;
+            }
+            else{
+                st.push(time);
+            }
+        }
+        return st.size();
+    }
+}
