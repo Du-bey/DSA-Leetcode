614d3fb386f214a6651eed7fd0030a903e91d4d1
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-20 00:06:30 +0530

    Time: 2 ms (77.20%) | Memory: 45.4 MB (69.59%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -4,7 +4,7 @@ class Solution {
         int maxi = 0;
         for(int i =0;i<n;i++){
             if(i > maxi) return false;
-            maxi = Math.max(maxi, i+ nums[i]);
+            maxi = Math.max(maxi, i + nums[i]);
         }
         return true;
     }
