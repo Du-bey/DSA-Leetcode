a0c57e3155b1839c1b39a7f75f33dd4b412fdb94
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-25 16:00:28 +0530

    Time: 10 ms (29.28%) | Memory: 54.4 MB (75.65%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,52 +2,52 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        Queue<Pair> q = new LinkedList<>();
-        boolean[][] vis = new boolean[n][m];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(i ==0 || j ==0 || i == n-1 || j == m-1){
-                    if(grid[i][j] == 1){
-                        q.add(new Pair(i, j));
-                        vis[i][j] =true;
-                    }
-                }
-            }
-        }
-        int[] delr = {0,-1,0,1};
-        int[] delc = {-1,0,1, 0};
-        while(!q.isEmpty()){
-            int r = q.peek().first;
-            int c = q.peek().second;
-            q.remove();
 
-            for(int i =0;i<4;i++){
-                int newr = r+delr[i];
-                int newc = c+delc[i];
-                if(newr >=0 && newc >=0 && newr<n && newc <m && !vis[newr][newc] && grid[newr][newc] == 1){
-                    vis[newr][newc] = true;
-                    q.add(new Pair(newr, newc));
-                }
-            }
-        }
-        int ans = 0;
-        for(int i =0;i<n;i++){
+        boolean[][] vis = new boolean[n][m];
+        Queue<Pair> q = new LinkedList<>();
+        for(int i = 0 ;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    ans++;
+                if((i == 0 || j == 0 || i == n-1 || j== m-1) && grid[i][j] == 1){
+                    vis[i][j] = true;
+                    q.add(new Pair(i, j));
                 }
             }
         }
-        return ans;
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+
+        while(!q.isEmpty()){
+            Pair p = q.peek();
+            q.remove();
+            int r = p.r;
+            int c = p.c;
+            for(int i =0;i<4;i++){
+                    int nr = r + dr[i];
+                    int nc = c + dc[i];
+                    if(nr >= 0 && nr < n && nc >=0 && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
+                        q.add(new Pair(nr, nc));
+                        vis[nr][nc] = true;
+                    }
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0 ;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(!vis[i][j] && grid[i][j] == 1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 }
 
-class Pair {
-    int first;
-    int second;
-
-    public Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
+class Pair{
+    int r;
+    int c;
+    public Pair(int r, int c){
+        this.r = r;
+        this.c = c;
+    }   
 }
