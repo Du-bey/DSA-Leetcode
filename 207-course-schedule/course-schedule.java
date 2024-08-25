013fe58b4bc908d5f9cb939e29a4f92e060d67d2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-25 23:55:45 +0530

    Time: 5 ms (66.73%) | Memory: 44.2 MB (84.06%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,48 +1,34 @@
 class Solution {
     public boolean canFinish(int n, int[][] prerequisites) {
-        int n1 = prerequisites.length;
         List<List<Integer>> adj = new ArrayList<>();
         for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i =0;i<n1;i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            adj.get(u).add(v);
-        }
-        boolean[] vis = new boolean[n];
-        
-        int[] tSort = new int[n];
-
-        int index =0;
         int[] indegree = new int[n];
-        for(int i =0;i<n;i++){
-            for(int it : adj.get(i)){
-                indegree[it]++;
-            }
+
+        for(int[] p : prerequisites){
+            int a = p[0];
+            int b = p[1];
+            adj.get(b).add(a);
+            indegree[a]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
         for(int i =0;i<n;i++){
             if(indegree[i] == 0){
                 q.add(i);
-                vis[i] = true;
             }
         }
+        int cnt = 0;
         while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
-            tSort[index++] = node;
-
-            for(int it : adj.get(node)){
-                indegree[it]--;
-                if(indegree[it] == 0){
-                    q.add(it);
-                    vis[it] = true;
+            int u = q.poll();
+            cnt++;
+            for(int v : adj.get(u)){
+                if(--indegree[v] == 0){
+                    q.add(v);
                 }
             }
         }
-        
-        return index == n;
+        return cnt == n;
     }
-
 }
