5fddfd7c2cc5bbe7cde26d52be350bea5517067d
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-29 13:45:04 +0530

    Time: 19 ms (24.93%) | Memory: 65.2 MB (42.22%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -5,15 +5,15 @@ class Solution {
         for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] edge : connections){
-            int u = edge[0];
-            int v = edge[1];
+        for(int[] r : connections){
+            int u = r[0];
+            int v = r[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
         boolean[] vis = new boolean[n];
         int isolatedNetwork =0;
+
         for(int i =0;i<n;i++){
             if(!vis[i]){
                 isolatedNetwork++;
@@ -21,15 +21,14 @@ class Solution {
             }
         }
         extraEdge /= 2;
-        System.out.println(isolatedNetwork +" " +extraEdge );
         if(isolatedNetwork-1 > extraEdge) return -1;
         return isolatedNetwork - 1;
     }
 
-    public void bfs(boolean[] vis, List<List<Integer>> adj, int node) {
+    public void bfs(boolean[] vis, List<List<Integer>> adj, int i){
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(node, -1));
-        vis[node] = true;
+        q.add(new Pair(i, -1));
+        vis[i] = true;
         while(!q.isEmpty()){
             int n = q.peek().first;
             int p = q.peek().second;
