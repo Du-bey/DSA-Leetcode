98fc0c5b75a3b1fc93f1c942318a535fa2cb7c3e
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-31 00:49:36 +0530

    Time: 8 ms (85.24%) | Memory: 46.4 MB (58.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,11 +2,7 @@ class Solution {
     public int[][] merge(int[][] in) {
         int n = in.length;
         List<int[]> ans = new ArrayList<>();
-        Arrays.sort(in, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
+        Arrays.sort(in, (a, b) -> a[0] - b[0]);
         for(int i =0;i<n;i++){
             if(i < n-1 && in[i][1] >= in[i+1][0]){
                 in[i+1][0] = in[i][0];
@@ -17,7 +13,6 @@ class Solution {
             else{
                 ans.add(in[i]);
             }
-            
         }
         return ans.toArray(new int[ans.size()][]);
     }
