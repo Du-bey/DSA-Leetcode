72792c9912ced1a49a61e7e8079ffdc326a82e68
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-08-31 17:28:36 +0530

    Time: 1 ms (98.10%) | Memory: 44.5 MB (99.01%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,21 +1,18 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return f(0, nums, n, dp);
-    }
-
-    public int f(int i, int[] nums, int n, int[] dp){
-        if(i >= n-1) return 0;
-        if(dp[i] != -1) return dp[i];
-        int ans = 100000000;
-        int x = nums[i];
-        for(int ind = i + 1;(ind <=  i + x && ind < n); ind++){
-            int cost = 1 + f(ind, nums, n, dp);
-            ans = Math.min(ans, cost);
+        int r = 0;
+        int l = 0;
+        int jumps = 0;
+        while(r < n -1){
+            int far = 0;
+            for(int i =l;i<=r;i++){
+                far = Math.max(far, i + nums[i]);
+            }
+            jumps++;
+            l = r + 1;
+            r = far;
         }
-        dp[i] = ans;
-        return dp[i];
+        return jumps;
     }
 }
