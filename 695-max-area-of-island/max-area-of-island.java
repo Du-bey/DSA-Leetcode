ea8277e7d3417ed28af7c8da09a8706bd69f37fe
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-02 14:13:38 +0530

    Time: 4 ms (15.27%) | Memory: 44.8 MB (32.56%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -2,31 +2,51 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        if(n == 1 && m == 1) return grid[0][0];
         boolean [][] vis = new boolean[n][m];
         int cnt = 0;
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(!vis[i][j] && grid[i][j] == 1){
-                    cnt = Math.max(cnt, dfs(i, j, vis, grid, n, m));
+                    int size = bfs(i, j, vis, grid, n, m);
+                    vis[i][j] = true;
+                    cnt = Math.max(cnt, size);
                 }
             }
         }
         return cnt;
     }
 
-    public int dfs(int i, int j, boolean [][] vis, int[][] grid, int n, int m){
-        int cnt = 0;
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 || vis[i][j]){
-            return 0;
-        }
-        cnt++;
+    public int bfs(int i, int j, boolean [][] vis, int[][] grid, int n, int m){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, j));
+        int cnt = 1;
+        int[] delr = {0, 1, 0, -1};
+        int[] delc = {1, 0, -1, 0};
         vis[i][j] = true;
-        cnt += dfs(i+1, j, vis, grid, n, m);
-        cnt += dfs(i-1, j, vis, grid, n, m);
-        cnt += dfs(i, j+1, vis, grid, n, m);
-        cnt += dfs(i, j-1, vis, grid, n, m);
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int r = p.r;
+            int c = p.c;
+            
+            for(int ind= 0;ind<4;ind++){
+                int nr = r + delr[ind];
+                int nc = c + delc[ind];
+                if(nr >=0 && nc >=0 && nr <n && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
+                    cnt++;
+                    q.add(new Pair(nr, nc));
+                    vis[nr][nc] = true;
+                }
+            }
+        }
         return cnt;
     }
 
+}
+class Pair{
+    int r;
+    int c;
+    public Pair(int r, int c){
+        this.r = r;
+        this.c = c;
+    }
 }
