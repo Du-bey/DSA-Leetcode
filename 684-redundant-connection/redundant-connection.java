955df80688cdd22f748dd14e134e96e42d7d8ab4
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-03 01:55:33 +0530

    Time: 4 ms (27.23%) | Memory: 45.2 MB (12.22%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,54 +1,35 @@
 class Solution {
-    int n;
-    int[] parent;
-    int[] rank;
     public int[] findRedundantConnection(int[][] edges) {
-        n = edges.length;
-        parent = new int[n+1];
-        rank = new int[n+1];
-        for(int i =0;i<n;i++){
-            parent[i] = i;
-            rank[i] = 1;
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
         }
-        int[] ans = new int[2];
-        for(int i =0;i<n;i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int up = findUPar(u);
-            int vp = findUPar(v);
-            if(!connect(up,vp)){
-                ans[0] = u;
-                ans[1] = v;
+        
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            boolean[] vis = new boolean[n+1];
+            if(dfs(u, v, adj, vis)){
+                return edge;
+            }
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
+        return new int[0];
+    }
+
+    public boolean dfs(int u, int v, List<List<Integer>> adj, boolean[] vis){
+        if(u == v) return true;
+        vis[u] = true;
+        for(int it : adj.get(u)){
+            if(!vis[it]){
+                if(dfs(it, v, adj, vis)){
+                    return true;
+                }
             }
         }
-    return ans;
-    }
-
-    public int findUPar(int node) {
-        int p = parent[node];
-        if(node == p) return node;
-        int uPar = findUPar(p);
-        parent[node] = uPar;
-        return uPar;
-    }
-
-    public boolean connect(int u, int v) {
-        int up = parent[u];
-        int vp = parent[v];
-        if(up == vp) return false;
-        int ru = rank[u];
-        int rv = rank[v];
-        if(ru > rv){
-            parent[vp] = up;
-            ru+=rv;
-        }
-        else if(ru < rv){
-            parent[up] = vp;
-        }
-        else{
-            parent[vp] = up;
-            rank[up]++;
-        }
-        return true;
+        return false;
     }
 }
