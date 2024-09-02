cf7cece1846ec3fb3e2c626b31685bc758d544df
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-02 22:01:40 +0530

    Time: 4 ms (19.53%) | Memory: 45.3 MB (63.36%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,44 +2,44 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-
-        boolean[][] vis = new boolean[n][m];
         Queue<Pair> q = new LinkedList<>();
-        for(int i = 0 ;i<n;i++){
+        boolean[][] vis = new boolean[n][m];
+        for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if((i == 0 || j == 0 || i == n-1 || j== m-1) && board[i][j] == 'O'){
-                    vis[i][j] = true;
-                    q.add(new Pair(i, j));
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(board[i][j] == 'O'){
+                        q.add(new Pair(i, j));
+                        vis[i][j] = true;
+                    }
                 }
             }
         }
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-
+        int[] delr = {0, 1, 0, -1};
+        int[] delc = {1, 0, -1, 0};
         while(!q.isEmpty()){
-            Pair p = q.peek();
-            q.remove();
+            Pair p = q.poll();
             int r = p.r;
             int c = p.c;
-            for(int i =0;i<4;i++){
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-                    if(nr >= 0 && nr < n && nc >=0 && nc < m && !vis[nr][nc] && board[nr][nc] == 'O'){
-                        q.add(new Pair(nr, nc));
-                        vis[nr][nc] = true;
-                    }
+            for(int i=0;i<4;i++){
+                int nr = r + delr[i];
+                int nc = c + delc[i];
+                if(nr>=0 && nc >=0 && nr < n && nc < m && board[nr][nc] == 'O' && !vis[nr][nc]){
+                    vis[nr][nc] = true;
+                    q.add(new Pair(nr, nc));
+                }
             }
         }
 
-        for(int i = 0 ;i<n;i++){
+        for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(!vis[i][j] && board[i][j] == 'O'){
+                if(board[i][j] == 'O' && !vis[i][j]){
                     board[i][j] = 'X';
                 }
             }
         }
     }
 }
+
 
 class Pair{
     int r;
