c0f9cac34f3709466f9ffde886c2310bc8815a6f
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-03 02:37:03 +0530

    Time: 1 ms (78.09%) | Memory: 43.4 MB (35.53%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,35 +1,51 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            adj.add(new ArrayList<>());
-        }
-        
+        DisjointSet ds = new DisjointSet(n+1);
         for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-            boolean[] vis = new boolean[n+1];
-            if(dfs(u, v, adj, vis)){
+            if(ds.unionBySize(edge[0], edge[1]) == false){
                 return edge;
             }
-            adj.get(u).add(v);
-            adj.get(v).add(u);
         }
-        
-        return new int[0];
+        return new int[1];
+    }
+}
+
+class DisjointSet{
+    List<Integer> par;
+    List<Integer> size;
+
+    public DisjointSet(int u){
+        par = new ArrayList<>();
+        size = new ArrayList<>();
+        for(int i =0;i<u;i++){
+            par.add(i);
+            size.add(1);
+        }
     }
 
-    public boolean dfs(int u, int v, List<List<Integer>> adj, boolean[] vis){
-        if(u == v) return true;
-        vis[u] = true;
-        for(int it : adj.get(u)){
-            if(!vis[it]){
-                if(dfs(it, v, adj, vis)){
-                    return true;
-                }
-            }
+    public boolean unionBySize(int u, int v){
+        int up = findUPar(u);
+        int vp = findUPar(v);
+        if(up == vp) return false;
+        int su = size.get(u);
+        int sv = size.get(v);
+        if(su > sv){
+            size.set(up, su + sv);
+            par.set(vp, up);
         }
-        return false;
+        else{
+            size.set(vp, su + sv);
+            par.set(up, vp);
+        }
+        return true;
+    }
+
+    public int findUPar(int u){
+        int p = par.get(u);
+        if(p == u) return u;
+        int up = findUPar(p);
+        par.set(u, up);
+        return up;
     }
 }
