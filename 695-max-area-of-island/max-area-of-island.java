f294f1fd5a3bd04a56b2a959922db096fafac1bf
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-02 14:08:05 +0530

    Time: 2 ms (67.57%) | Memory: 44.4 MB (59.64%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -8,47 +8,25 @@ class Solution {
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(!vis[i][j] && grid[i][j] == 1){
-                    int size = bfs(i, j, vis, grid, n, m);
-                    vis[i][j] = true;
-                    cnt = Math.max(cnt, size);
+                    cnt = Math.max(cnt, dfs(i, j, vis, grid, n, m));
                 }
             }
         }
         return cnt;
     }
 
-    public int bfs(int i, int j, boolean [][] vis, int[][] grid, int n, int m){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i, j));
+    public int dfs(int i, int j, boolean [][] vis, int[][] grid, int n, int m){
         int cnt = 0;
-        int[] delr = {0, 1, 0, -1};
-        int[] delc = {1, 0, -1, 0};
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int r = p.r;
-            int c = p.c;
-            if(vis[r][c]) continue;
-            vis[r][c] = true;
-            cnt++;
-            
-            for(int ind= 0;ind<4;ind++){
-                int nr = r + delr[ind];
-                int nc = c + delc[ind];
-                if(nr >=0 && nc >=0 && nr <n && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
-                    
-                    q.add(new Pair(nr, nc));
-                }
-            }
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0 || vis[i][j]){
+            return 0;
         }
+        cnt++;
+        vis[i][j] = true;
+        cnt += dfs(i+1, j, vis, grid, n, m);
+        cnt += dfs(i-1, j, vis, grid, n, m);
+        cnt += dfs(i, j+1, vis, grid, n, m);
+        cnt += dfs(i, j-1, vis, grid, n, m);
         return cnt;
     }
 
-}
-class Pair{
-    int r;
-    int c;
-    public Pair(int r, int c){
-        this.r = r;
-        this.c = c;
-    }
 }
