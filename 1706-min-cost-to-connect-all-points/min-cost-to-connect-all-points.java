f8d678685fccba1ed3c1cf9675931c766c9fc829
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-04 01:12:56 +0530

    Time: 112 ms (63.17%) | Memory: 63 MB (40.16%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,62 +1,32 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        Set<Tuple> set = new HashSet<>();
-        PriorityQueue<Tuple> queue = new PriorityQueue<>((t1,t2) -> t1.dist - t2.dist);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]); 
+        pq.offer(new int[] { 0, 0 });
+        int len = points.length;
+        Set<Integer> visited = new HashSet<>();
+        int cost = 0;
 
-        int n = points.length;
-        if(n == 0) return 0;
+        while (visited.size() < len) {
+            int[] arr = pq.poll();
 
-        set.add(new Tuple(points[0][0],points[0][1],0));
-        queue.offer(new Tuple(points[0][0],points[0][1],0));
+            int weight = arr[0];
+            int currNode = arr[1];
 
-        while(!queue.isEmpty()){
-            Tuple t = queue.poll();
-            if(!set.contains(t)) set.add(t);
-            if(set.size() == n) break;
-            
-            int x = t.x;
-            int y = t.y;
-            int dist = t.dist;
+            if (visited.contains(currNode)) continue;
 
-            for(int i=0;i<n;i++){
-                int[] point = points[i];
-                int newDist = Math.abs(point[0] - x) + Math.abs(point[1] - y);
-                Tuple newT = new Tuple(point[0],point[1],newDist);
-                if(!set.contains(newT)){
-                    queue.offer(newT);
+            visited.add(currNode);
+            cost += weight;
+
+            for (int nextNode = 0; nextNode < len; nextNode++) {
+                if (!visited.contains(nextNode)) {
+                    int nextWeight =
+                        Math.abs(points[nextNode][0] - points[currNode][0]) +
+                        Math.abs(points[nextNode][1] - points[currNode][1]);
+                    pq.offer(new int[] { nextWeight, nextNode });
                 }
             }
         }
 
-        int totalEffort = 0;
-        for(Tuple t : set){
-            totalEffort += t.dist;
-        }
-
-        return totalEffort;
-    }
-    private class Tuple{
-        int x;
-        int y;
-        int dist;
-        public Tuple(int x,int y,int dist){
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Tuple tuple = (Tuple) o;
-            return x == tuple.x && y == tuple.y; 
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
-    }
+        return cost;
+    }   
 }
-
