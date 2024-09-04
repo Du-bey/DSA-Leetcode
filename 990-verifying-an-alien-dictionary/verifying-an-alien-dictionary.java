cd9da03b0783cee4c97c381ca671d96f18d0bd39
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-05 00:10:53 +0530

    Time: 1 ms (57.40%) | Memory: 42.1 MB (49.25%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.java
@@ -11,22 +11,19 @@ class Solution {
         for(int i =0;i<n-1;i++){
             String s = words[i];
             String t = words[i+1];
-            int min = Math.min(s.length(), t.length());
-            boolean dif = false;
-            for(int ind = 0;ind < min;ind++){
-                
-                char c1 = s.charAt(ind);
-                char c2 = t.charAt(ind);
+            int w1 = s.length();
+            int w2 = t.length();
+            for(int j =0;j<w1;j++){
+                if(j == w2) return false;
+                char c1 = s.charAt(j);
+                char c2 = t.charAt(j);
                 if(c1 != c2){
-                    dif = true;
                     if(h.get(c1) > h.get(c2)){
-                        System.out.println(c1 + " " + c2);
                         return false;
                     }
                     break;
                 }
             }
-            if(!dif && t.length() == min && s.length() > min) return false;
         }
         return true;
     }
