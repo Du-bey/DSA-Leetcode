896ce99ecc00b5678442e83db1be386ad259abf6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-04 16:24:47 +0530

    Time: 3 ms (16.60%) | Memory: 42.1 MB (88.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,40 +1,40 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        Queue<Trip> q = new LinkedList<>();
+    public int orangesRotting(int[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        Queue<Pair> q = new LinkedList<>();
         boolean[][] vis = new boolean[n][m];
-        int[] delr = {0, 1, 0, -1};
-        int[] delc = {1, 0, -1, 0};
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == 2){
-                    q.add(new Trip(i, j, 0));
-                    vis[i][j] = true;
+                if(board[i][j] == 2){
+                    q.add(new Pair(i, j, 0));
                 }
             }
         }
+        int[] delr = {0, 1, 0, -1};
+        int[] delc = {1, 0, -1, 0};
         int ans = 0;
         while(!q.isEmpty()){
-            Trip p = q.poll();
+            Pair p = q.poll();
             int r = p.r;
             int c = p.c;
             int t = p.t;
+            if(vis[r][c]) continue;
+            vis[r][c] = true;
             ans = Math.max(ans, t);
-            for(int i =0;i<4;i++){
+            for(int i=0;i<4;i++){
                 int nr = r + delr[i];
                 int nc = c + delc[i];
-                if(nr >= 0 && nc >= 0 && nr < n && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
-                    vis[nr][nc] = true;
-                    q.add(new Trip(nr, nc, t+1));
-                    grid[nr][nc] = 2;
+                if(nr>=0 && nc >=0 && nr < n && nc < m && board[nr][nc] == 1){
+                    q.add(new Pair(nr, nc, t+1));
+                    board[nr][nc] = 2;
                 }
             }
         }
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == 1){
+                if(board[i][j] == 1){
                     return -1;
                 }
             }
@@ -43,13 +43,14 @@ class Solution {
     }
 }
 
-class Trip{
+
+class Pair{
     int r;
     int c;
     int t;
-    public Trip(int r, int c, int t){
+    public Pair(int r, int c, int t){
         this.r = r;
         this.c = c;
         this.t = t;
-    }
+    }   
 }
