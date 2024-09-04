6e3a2439940fb3cb4e98bf4bb36d2c983e56203e
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-04 22:41:43 +0530

    Time: 6 ms (83.61%) | Memory: 44.1 MB (75.36%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,36 +1,36 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++){
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new Pair(flight[1], flight[2]));
+        for(int[] flight : flights){
+            int u = flight[0];
+            int v = flight[1];
+            int w = flight[2];
+            adj.get(u).add(new Pair(v, w));
         }
         int[] dis = new int[n];
         Arrays.fill(dis, Integer.MAX_VALUE);
         dis[src] = 0;
+
         Queue<Triplet> q = new LinkedList<>();
         q.add(new Triplet(0, src, 0));
 
         while(!q.isEmpty()){
-            int stop = q.peek().first;
-            int node = q.peek().second;
-            int cost = q.peek().third;
-            q.remove();
-
+            Triplet t = q.poll();
+            int stop = t.first;
+            int u = t.second;
+            int wt = t.third;
             if(stop > k) continue;
-
-            for(Pair iter : adj.get(node)){
-                int adjNode = iter.first;
-                int edW = iter.second;
-                if(edW+cost < dis[adjNode]){
-                    dis[adjNode] = edW+cost;
-                    q.add(new Triplet(stop+1, adjNode, dis[adjNode]));
+            for(Pair p : adj.get(u)){
+                int v = p.first;
+                int edw = p.second;
+                if(dis[v] > wt + edw){
+                    dis[v] = wt + edw;
+                    q.add(new Triplet(stop + 1, v, dis[v]));
                 }
             }
-
         }
         return dis[dst] == Integer.MAX_VALUE ? -1 : dis[dst];
     }
