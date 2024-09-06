518718d8ecad53b1bf53234f376e1d985ec836a2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-07 03:15:00 +0530

    Time: 18 ms (58.82%) | Memory: 66.7 MB (35.29%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.java
@@ -1,35 +1,29 @@
 class Solution {
     public int closestMeetingNode(int[] edges, int node1, int node2) {
         int n = edges.length;
-        int[] ans = new int[n];
-        boolean[] vis = new boolean[n];
-        Arrays.fill(ans, Integer.MAX_VALUE);
-        dfs(node1, ans, vis, edges, 0);
+        int[] dis1 = new int[n];
+        int[] dis2 = new int[n];
+        boolean[] vis1 = new boolean[n];
         boolean[] vis2 = new boolean[n];
-        dfs(node2, ans, vis2, edges, 0);
-        int res = Integer.MAX_VALUE;
+        dfs(node1, dis1, vis1, edges, 0);
+        dfs(node2, dis2, vis2, edges, 0);
+        
         int ret = -1;
+        int minDis = Integer.MAX_VALUE;
         for(int i =0;i<n;i++){
-            if(ans[i] < res && vis[i] && vis2[i]){
+            if((minDis > Math.max(dis1[i], dis2[i])) && vis1[i] && vis2[i]){
+                minDis = Math.max(dis1[i], dis2[i]);
                 ret = i;
-                res = ans[i];
             }
-
         }
         return ret;
     }
 
     public void dfs(int u, int[] ans, boolean[] vis, int[] edges, int dis){
-        if(vis[u]) return;
         vis[u] = true;
-        if(ans[u] == Integer.MAX_VALUE){
-            ans[u] = dis;
-        }
-        else {
-            ans[u] = Math.max(ans[u], dis);
-        }
         int v = edges[u];
-        if(v != -1){
+        if(v != -1 && !vis[v]){
+            ans[v] = dis + 1;
             dfs(v, ans, vis, edges, dis + 1);
         }
     }
