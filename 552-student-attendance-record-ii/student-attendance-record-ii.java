56f80b0838310e518289db911ca4f232c142d5ee
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-07 19:18:14 +0530

    Time: 234 ms (73.25%) | Memory: 59.6 MB (65.82%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.java
+++ 552-student-attendance-record-ii/student-attendance-record-ii.java
@@ -2,23 +2,23 @@ class Solution {
     final int mod = 1000000007;
     public int checkRecord(int n) {
         int[][][] dp = new int[n+1][2][3];
-        for(int[][] r : dp){
-            for(int[] r2 : r){
-                Arrays.fill(r2, -1);
+        for(int i =0;i<2;i++){
+            for(int j =0;j<3;j++){
+                dp[0][i][j] = 1;
             }
         }
-        return f(n, 0, 0, dp);
+        for(int i =1;i<=n;i++){
+            for(int a=1;a>=0;a--){
+                for(int l =2;l>=0;l--){
+                    int ab = dp[i-1][a][0] % mod;
+                    if(a < 1) ab = (ab + dp[i-1][a+1][0]) % mod;
+                    if(l < 2) ab = (ab + (dp[i-1][a][l+1]) % mod);
+                    
+                    dp[i][a][l] = (ab) % mod;
+                }
+            }
+        }
+        return dp[n][0][0] % mod;
     }
 
-    public int f(int i, int a, int l, int[][][] dp){
-        if(i == 0) return 1;
-        if(dp[i][a][l] == -1){
-            int ab = f(i-1, a ,0, dp) % mod;
-            if(a < 1) ab = (ab + f(i-1, a+1, 0, dp)) % mod;
-            if(l < 2) ab = (ab + (f(i-1, a, l+1, dp)) % mod);
-            
-            dp[i][a][l] = (ab) % mod;
-        }
-        return dp[i][a][l] % mod;
-    }
 }
