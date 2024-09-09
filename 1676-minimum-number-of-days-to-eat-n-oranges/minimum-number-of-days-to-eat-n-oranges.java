02c47d838164a4c03fe7306cd302132060a7beba
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-09 19:33:01 +0530

    Time: 3 ms (99.36%) | Memory: 42.2 MB (57.96%) - LeetSync

--- 1676-minimum-number-of-days-to-eat-n-oranges/minimum-number-of-days-to-eat-n-oranges.java
+++ 1676-minimum-number-of-days-to-eat-n-oranges/minimum-number-of-days-to-eat-n-oranges.java
@@ -1,14 +1,10 @@
 class Solution {
+    Map<Integer, Integer> dp = new HashMap<>();
     public int minDays(int n) {
-        Map<Integer, Integer> dp = new HashMap<>();
-        return f(n, dp);
-    }
-
-    public int f(int n, Map<Integer, Integer> dp){
-        if(n <= 1) return n;
+        if(n <= 2) return n;
         if(dp.containsKey(n)) return dp.get(n);
-        int a = (n % 2) + f(n/2, dp);
-        int b = (n % 3) + f(n/3, dp);
+        int a = (n % 2) + minDays(n/2);
+        int b = (n % 3) + minDays(n/3);
         dp.put(n, 1 + Math.min(a, b));
         return dp.get(n);
     }
