22c8d31f1afdbbefcc58c9b0b272014fee569914
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-09 18:04:49 +0530

    Time: 16 ms (99.30%) | Memory: 40.9 MB (77.98%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -2,13 +2,28 @@ class Solution {
     public int getMaximumGold(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        int count = checkIfAllNonZero(grid, n, m);
+        if(count != 0) return count;
         int ans = 0;
         for(int i =0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans = Math.max(ans, dfs(i, j, new boolean[n][m], grid, n, m));
+                if(grid[i][j] !=0){
+                    ans = Math.max(ans, dfs(i, j, new boolean[n][m], grid, n, m));
+                }
             }
         }
         return ans;
+    }
+
+    public int checkIfAllNonZero(int[][] grid, int n, int m){
+        int cnt = 0;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] !=0) cnt += grid[i][j];
+                else return 0;
+            }
+        }
+        return cnt;
     }
 
     public int dfs(int i, int j, boolean[][] vis, int[][] grid, int n, int m){
