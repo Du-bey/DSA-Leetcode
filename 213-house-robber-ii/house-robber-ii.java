27e4db3341cb8866420c2cc6082bba1ce0e20a39
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-11 03:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (52.54%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,25 +1,21 @@
 class Solution {
-    int dp[][];
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
 
-        dp = new int[n+1][2];
-        for(int i[] : dp)Arrays.fill(i,-1);
-
-        int a = f(nums, 0, n-2);
-        int b = f(nums, 1, n-1);
-        return Math.max(a,b);
+        return Math.max(rob1(nums, 0, n-2), rob1(nums, 1, n-1));
     }
-    public int f(int[] nums, int st, int i){
-        if(i < st) return 0;
-        if(i == st) return nums[st];
 
-        if(dp[i][st] != -1)return dp[i][st];
-
-        int a = nums[i] + f(nums, st, i-2);
-        int b = f(nums, st, i-1);
-        dp[i][st] = Math.max(a, b);
-        return dp[i][st];
+    public int rob1(int[] nums, int l, int r) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int pre = 0;
+        int pre2 = 0;
+        for(int i =l;i<=r;i++){
+            int cur = Math.max(pre, nums[i] + pre2);
+            pre2 = pre;
+            pre = cur;
+        }
+        return pre;
     }
 }
