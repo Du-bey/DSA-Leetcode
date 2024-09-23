44f10ddb3e517afa843a710158df915c760a9803
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-23 23:26:13 +0530

    Time: 77 ms (8.84%) | Memory: 45.6 MB (13.27%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -3,23 +3,17 @@ class Solution {
         HashSet<String> d = new HashSet<>();
         for(String s1 : dictionary) d.add(s1);
         int n = s.length();
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return f(0, d, s, dp);
-    }
-
-    public int f(int i, HashSet<String> d, String s, int[] dp){
-        int n = s.length();
-        if(i >= n) return 0;
-        if(dp[i] != -1) return dp[i];
-        dp[i] = 1 + f(i+1, d, s, dp);
-        String t = "";
-        for(int j =i;j<n;j++){
-            t += s.charAt(j);
-            if(d.contains(t)){
-                dp[i] = Math.min(dp[i], f(j + 1, d, s, dp));
+        int[] dp = new int[n+1];
+        for(int i = n-1;i>=0;i--){
+            dp[i] = 1 + dp[i+1];
+            String t = "";
+            for(int j =i;j<n;j++){
+                t += s.charAt(j);
+                if(d.contains(t)){
+                    dp[i] = Math.min(dp[i], dp[j+1]);
+                }
             }
         }
-        return dp[i];
+        return dp[0];
     }
 }
