c8ce7cf21bbe69f90e518e9c2ab6de9850529e2d
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-25 15:56:29 +0530

    Time: 36 ms (61.38%) | Memory: 60.5 MB (5.06%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -9,22 +9,22 @@ class Trie {
         Node node = root;
         for(int i =0;i<word.length();i++){
             char ch = word.charAt(i);
-            if(!node.containsKey(ch)){
-                node.put(ch, new Node());
+            if(node.links[ch - 'a'] == null){
+                node.links[ch - 'a'] = new Node();
             }
-            node = node.get(ch);
+            node = node.links[ch - 'a'];
         }
-        node.setEnd();
+        node.flag = true;
     }
     
     public boolean search(String word) {
         Node node = root;
         for(int i =0;i<word.length();i++){
             char ch = word.charAt(i);
-            if(!node.containsKey(ch)){
+            if(node.links[ch - 'a'] == null){
                 return false;
             }
-            node = node.get(ch);
+            node = node.links[ch - 'a'];
         }
         return node.flag;
     }
@@ -33,10 +33,10 @@ class Trie {
         Node node = root;
         for(int i =0;i<prefix.length();i++){
             char ch = prefix.charAt(i);
-            if(!node.containsKey(ch)){
+            if(node.links[ch - 'a'] == null){
                 return false;
             }
-            node = node.get(ch);
+            node = node.links[ch - 'a'];
         }
         return true;
     }
@@ -45,24 +45,6 @@ class Trie {
 class Node {
     Node[] links = new Node[26];
     boolean flag = false;
-
-    public Node() {};
-
-    public boolean containsKey(char ch){
-        return links[ch - 'a'] != null;
-    }
-
-    public void put(char ch, Node node){
-        links[ch - 'a'] = node;
-    }
-
-    public Node get(char ch){
-        return links[ch - 'a'];
-    }
-    
-    public void setEnd(){
-        flag = true;
-    }
 }
 
 /**
