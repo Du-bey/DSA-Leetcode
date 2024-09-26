c772d453d53badbed54823966741ba9d77495e41
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-09-26 18:51:50 +0530

    Time: 785 ms (46.69%) | Memory: 185 MB (25.52%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        Node root = new Node();
+        for(int n : nums){
+            Node node = root;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (n >> i) & 1; 
+                if(node.links[bit] == null){
+                    node.links[bit] = new Node();
+                }
+                node = node.links[bit];
+            }
+        }
+        int ans = 0;
+        for(int n : nums){
+            Node node = root;
+            int max = 0;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (n >> i) & 1; 
+                if(node.links[1 - bit] != null){
+                    max |= (1 << i);
+                    node = node.links[1 - bit];
+                }
+                else{
+                    node = node.links[bit];
+                }
+                
+            }
+            ans = Math.max(ans, max);
+        }
+        return ans;
+    }
+}
+
+class Node{
+    Node[] links = new Node[2];
+}
