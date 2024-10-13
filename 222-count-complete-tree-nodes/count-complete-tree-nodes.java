6388f67b9baad79ec6fa414265aea3762ae9d59e
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-10-13 15:38:01 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (27.33%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -16,6 +16,28 @@
 class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
+        int lh = findlh(root);
+        int rh = findrh(root);
+
+        if(lh == rh) return ((1 << lh) - 1);
         return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+
+    public int findlh(TreeNode root){
+        int cnt = 0;
+        while(root != null){
+            cnt++;
+            root = root.left;
+        }
+        return cnt;
+    }
+
+    public int findrh(TreeNode root){
+        int cnt = 0;
+        while(root != null){
+            cnt++;
+            root = root.right;
+        }
+        return cnt;
     }
 }
