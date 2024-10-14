33d707509d1f1bf92f3bd25157e1c4eb03282f28
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-10-14 22:21:24 +0530

    Time: 30 ms (24.06%) | Memory: 46.3 MB (12.66%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,27 +1,22 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        HashMap<Integer, Integer> h = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
         for(int no : arr){
-            pq.add(Math.abs(no - x));
-            if(pq.size() > k) pq.poll();
-            h.put(no, h.getOrDefault(no, 0) + 1);
+            if(k-- > 0){
+                pq.add(no);
+            }
+            else if(Math.abs(pq.peek() - x) > Math.abs(no - x)){
+                pq.poll();
+                pq.add(no);
+            }
+            
         }
         List<Integer> ans = new ArrayList<>();
         while(!pq.isEmpty()){
             int no = pq.poll();
-            int b = x - no;
-            int a = x + no;
-            if(h.containsKey(b) && h.get(b) > 0){
-                h.put(b, h.get(b) - 1);
-                ans.add(b);
-            }
-            else if(h.containsKey(a) && h.get(a) > 0){
-                h.put(a, h.get(a) - 1);
-                ans.add(a);
-            }
+            ans.add(no);
         }
-        Collections.sort(ans);
         return ans;
     }
 }
