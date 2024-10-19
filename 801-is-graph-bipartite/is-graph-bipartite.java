d4ef5c68c94f19cda7a06b490807094544f3dad5
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-10-19 18:13:36 +0530

    Time: 1 ms (62.70%) | Memory: 44.8 MB (67.69%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-
+        List<List<Integer>> adj = new ArrayList<>();
         int n = graph.length;
         int[] col = new int[n];
         Arrays.fill(col, -1);
+
         for(int i =0;i<n;i++){
             if(col[i] == -1){
-                if(check(graph, i, col) == false) return false;
+                if(bfs(i, col, graph) == false) return false;
             }
         }
         return true;
     }
 
-    public boolean check(int[][] graph, int node, int[] col){
-        col[node] = 0;
+    public boolean bfs(int i, int[] col, int[][] graph){
+        col[i] = 0;
         Queue<Integer> q = new LinkedList<>();
-        q.add(node);
+        q.add(i);
         while(!q.isEmpty()){
-            int u = q.peek();
-            q.remove();
+            int u = q.poll();
             for(int v : graph[u]){
                 if(col[v] == -1){
                     col[v] = 1 - col[u];
                     q.add(v);
                 }
-                else if(col[v] == col[u]){
-                    return false;
+                else{
+                    if(col[v] == col[u]) return false;
                 }
             }
         }
         return true;
+        
     }
 }
