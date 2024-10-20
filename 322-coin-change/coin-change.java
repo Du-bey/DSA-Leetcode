279fb1b5c3bfe7c5356417c8f64e43a3f8632fc6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-10-21 00:49:32 +0530

    Time: 34 ms (18.25%) | Memory: 45.6 MB (9.88%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,11 +2,11 @@ class Solution {
     int mod = 100000000;
     public int coinChange(int[] coins, int t) {
         int n = coins.length;
-        int[][] dp = new int[n][t+1];
+        int[][] dp = new int[n+1][t+1];
         for(int[] row : dp){
             Arrays.fill(row, -1);
         }
-        int ans = f(n-1, t, coins, dp);
+        int ans = f(n, t, coins, dp);
         if(ans >= mod){
             return -1;
         }
@@ -14,14 +14,14 @@ class Solution {
     }
 
     public int f(int ind, int t, int[] coins, int[][] dp){
-        if(ind < 0){
+        if(ind == 0){
             return t == 0 ? 0 : mod;
         }
         if(dp[ind][t] != -1) return dp[ind][t];
         int notTake = f(ind-1, t, coins, dp);
         int take = mod;
-        if(coins[ind] <= t){
-            take = 1 + f(ind, t-coins[ind], coins, dp);
+        if(coins[ind-1] <= t){
+            take = 1 + f(ind, t-coins[ind-1], coins, dp);
         }
         dp[ind][t] = Math.min(take, notTake);
         return dp[ind][t];
