4a554385a123bbaf964fde23966e3487d468486e
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-02 01:24:58 +0530

    Time: 1 ms (68.23%) | Memory: 41.1 MB (94.35%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,31 +1,16 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
         int n = strs.length;
-        if(n == 1) return strs[0];
-        StringBuilder sb = new StringBuilder();
         String a = strs[0];
-        String b = strs[1];
-        int len = Math.min(a.length(), b.length());
-        for(int i =0;i<len;i++){
-            if(a.charAt(i) == b.charAt(i)){
-                sb.append(a.charAt(i));
+        String b = strs[n-1];
+        for(int i =0;i<a.length();i++){
+            if(a.charAt(i) != b.charAt(i)){
+                break;
             }
-            else break;
+            ans.append(a.charAt(i));
         }
-        if(sb.toString().equals("")) return "";
-        for(int i =2;i<n;i++){
-            String a1 = sb.toString();
-            String b1 = strs[i];
-            if(b1.equals("")) return b1;
-            int len1 = Math.min(a1.length(), b1.length());
-            for(int i1 =0;i1<len1;i1++){
-                if(a1.charAt(i1) != b1.charAt(i1)){
-                    sb.setLength(i1);
-                    break;
-                }
-            }
-            sb.setLength(Math.min(len1, sb.length()));
-        }
-        return sb.toString();
+        return ans.toString();
     }
 }
