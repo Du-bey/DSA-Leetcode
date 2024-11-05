0e9c3c603f44634c567aa934435b6567b648b361
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-05 14:18:39 +0530

    Time: 12 ms (86.02%) | Memory: 45.8 MB (32.97%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -4,14 +4,28 @@ class Solution {
         for(char c : s.toCharArray()){
             h.put(c, h.getOrDefault(c, 0) + 1);
         }
-        List<Character> l = new ArrayList(h.keySet());
-        Collections.sort(l, (a, b) -> h.get(b) - h.get(a));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> y.b - x.b);
+        for(Character c : h.keySet()){
+            pq.add(new Pair(c, h.get(c)));
+        }
         StringBuilder sb = new StringBuilder();
-        for(Character c : l){
-            for(int i =0;i<h.get(c);i++){
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
+            char c = p.a;
+            int freq = p.b;
+            for(int i =0;i<freq;i++){
                 sb.append(c);
             }
         }
         return sb.toString();
+    }
+}
+
+class Pair{
+    char a;
+    int b;
+    public Pair(char a, int b){
+        this.a = a;
+        this.b = b;
     }
 }
