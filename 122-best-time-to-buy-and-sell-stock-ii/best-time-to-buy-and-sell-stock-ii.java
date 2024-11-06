1a0a7c645e364a7f5bbf227cc53a8def51ecd1a8
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-07 01:11:34 +0530

    Time: 3 ms (10.28%) | Memory: 49.3 MB (5.03%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,23 @@
 class Solution {
-    public int maxProfit(int[] pr) {
-        int n = pr.length;
-        int nextBuy = 0;
-        int nextNotBuy = 0;
-        int curBuy = 0;
-        int curNotBuy = 0;
-        for(int ind = n-1; ind>= 0; ind--){
-            curBuy = Math.max(-pr[ind] + nextNotBuy, nextBuy);
-            curNotBuy = Math.max(pr[ind] + nextBuy, nextNotBuy);
-            nextBuy = curBuy;
-            nextNotBuy = curNotBuy;
+    public int maxProfit(int[] p) {
+        int n = p.length;
+        int[][] dp = new int[n][2];
+        for(int[] r : dp){
+            Arrays.fill(r, -1);
         }
-        return nextBuy;
+        return f(0, 1, n, p, dp);
+    }
+
+    public int f(int i, int buy, int n, int[] p, int[][] dp){
+        if(i == n) return 0;
+        if(dp[i][buy] != -1) return dp[i][buy];
+        int ans = 0;
+        if(buy == 1){
+            ans = Math.max(-p[i] + f(i+1, 0, n, p, dp), f(i+1, 1, n, p, dp));
+        }
+        else{
+            ans = Math.max(p[i] + f(i+1, 1, n, p, dp), f(i+1, 0, n, p, dp));
+        }
+        return dp[i][buy] = ans;
     }
 }
