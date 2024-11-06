9df9facdb47cc38acbdce68b528d4e6bdb8ca4bb
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-06 19:22:17 +0530

    Time: 14 ms (6.74%) | Memory: 43 MB (27.00%) - LeetSync

--- 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.java
+++ 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.java
@@ -1,12 +1,19 @@
 class Solution {
     public int numDifferentIntegers(String word) {
-        String replaced = word.replaceAll("[a-z]", " ");
-        Set<String> nums = new HashSet();
-        String [] words = replaced.split(" ");
-        for (String s : words) {
-            if (s.trim().length() > 0)
-                nums.add(s.replaceAll("^0*", ""));
+        int n = word.length();
+        StringBuilder sb = new StringBuilder();
+        HashSet<String> h = new HashSet<>();
+        for(int i =0;i<n;i++){
+            char ch = word.charAt(i);
+            if(ch - '0' >= 0 && ch - '0' < 10) sb.append(ch);
+            else{
+                String s = sb.toString();
+                if(!s.equals("")) h.add(s.replaceAll("^0*", ""));
+                sb.setLength(0);
+            }
         }
-        return nums.size();
+        String s = sb.toString();
+        if(!s.equals("")) h.add(s.replaceAll("^0*", ""));
+        return h.size();
     }
 }
