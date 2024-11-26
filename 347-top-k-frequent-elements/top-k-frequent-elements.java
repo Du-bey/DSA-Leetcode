2e27492c09a25bf574c671f3ebad582b68ced5b3
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-27 00:06:51 +0530

    Time: 11 ms (92.04%) | Memory: 45.5 MB (86.95%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,16 +1,27 @@
-class Solution {
+class Solution {    
     public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
         HashMap<Integer, Integer> h = new HashMap<>();
-        for(int num : nums){
-            h.put(num, h.getOrDefault(num, 0) + 1);
+        for(int x : nums){
+            h.put(x, h.getOrDefault(x, 0) + 1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> h.get(b) - h.get(a));
-        for(int key : h.keySet()){
-            pq.add(key);
+        List<Integer> bucket[] = new ArrayList[n+1];
+        for(int x : h.keySet()){
+            int freq = h.get(x);
+            if(bucket[freq] == null){
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(x);
         }
         int[] ans = new int[k];
-        for(int i =0;i<k;i++){
-            ans[i] = pq.poll();
+        int ind = 0;
+        for(int i =n;i>=0;i--){
+            if(bucket[i] != null){
+                for(int val : bucket[i]){
+                    ans[ind++] = val;
+                    if(ind == k) return ans;
+                }
+            }
         }
         return ans;
     }
