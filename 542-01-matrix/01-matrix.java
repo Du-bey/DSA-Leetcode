60a628bce4c8b71b534908bcf29cd263ba19cd64
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-30 13:52:49 +0530

    Time: 16 ms (32.73%) | Memory: 47.6 MB (27.27%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,50 +1,57 @@
 class Solution {
-    public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] ans = new int[n][m];
-        boolean[][] vis = new boolean[n][m];
-        Queue<Pair> q = new LinkedList<>();
-        for(int i = 0 ;i<n;i++){
-            for(int j =0;j<m;j++){
-                ans[i][j] = Integer.MAX_VALUE;
-                if(mat[i][j] == 0){
-                    ans[i][j] = 0;
-                    vis[i][j] = true;
-                    q.add(new Pair(i, j, 0));
-                }
-            }
-        }
+    public int[][] updateMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
 
+        Queue<Triplet> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][m];
+
+        int[][] ans = new int[n][m];
+        for(int [] r : ans){
+            Arrays.fill(r, Integer.MAX_VALUE);
+        }
+
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j] == 0){
+                    ans[i][j] = 0;
+                    q.add(new Triplet(0, i, j));
+                    vis[i][j] = true;
+                }
+            }
+        }
+
         while(!q.isEmpty()){
-            Pair p = q.peek();
-            q.remove();
-            int r = p.r;
-            int c = p.c;
-            int l = p.l;
+            Triplet trip = q.poll();
+            int t = trip.a;
+            int r = trip.b;
+            int c = trip.c;
+
             for(int i =0;i<4;i++){
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-                    if(nr >= 0 && nr < n && nc >=0 && nc < m && !vis[nr][nc] && mat[nr][nc] == 1){
-                        ans[nr][nc] = l+1;
-                        q.add(new Pair(nr, nc, l+1));
-                        vis[nr][nc] = true;
-                    }
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >=0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && !vis[nr][nc]){
+                    q.add(new Triplet(t + 1, nr, nc));
+                    ans[nr][nc] = Math.min(ans[nr][nc], t+1);
+                    vis[nr][nc] = true;
+                }
             }
         }
         return ans;
     }
 }
 
-class Pair{
-    int r;
+public class Triplet {
+    int a;
+    int b;
     int c;
-    int l;
-    public Pair(int r, int c, int l){
-        this.r = r;
+
+    public Triplet(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
         this.c = c;
-        this.l = l;
     }
 }
