15572fb913d2f4d794987102b6f807e152601c06
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-11-30 14:30:19 +0530

    Time: 4 ms (22.08%) | Memory: 45.4 MB (52.31%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,11 +2,15 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
+
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+
         Queue<Pair> q = new LinkedList<>();
         boolean[][] vis = new boolean[n][m];
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                if(i == 0 || i == n-1 || j == 0 || j == m-1){
                     if(board[i][j] == 'O'){
                         q.add(new Pair(i, j));
                         vis[i][j] = true;
@@ -14,22 +18,20 @@ class Solution {
                 }
             }
         }
-        int[] delr = {0, 1, 0, -1};
-        int[] delc = {1, 0, -1, 0};
+
         while(!q.isEmpty()){
             Pair p = q.poll();
-            int r = p.r;
-            int c = p.c;
-            for(int i=0;i<4;i++){
-                int nr = r + delr[i];
-                int nc = c + delc[i];
-                if(nr>=0 && nc >=0 && nr < n && nc < m && board[nr][nc] == 'O' && !vis[nr][nc]){
-                    vis[nr][nc] = true;
+            int r = p.a;
+            int c = p.b;
+            for(int i =0;i<4;i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if(nr >=0 && nr < n && nc >= 0 && nc < m && board[nr][nc] == 'O'  && !vis[nr][nc]){
                     q.add(new Pair(nr, nc));
+                    vis[nr][nc] = true;
                 }
             }
         }
-
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(board[i][j] == 'O' && !vis[i][j]){
@@ -40,12 +42,12 @@ class Solution {
     }
 }
 
+public class Pair {
+    int a;
+    int b;
 
-class Pair{
-    int r;
-    int c;
-    public Pair(int r, int c){
-        this.r = r;
-        this.c = c;
-    }   
+    public Pair(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
 }
