d41e2ab4c3dcae9cfcef429161ec9ab9f49f2e92
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-08 18:11:13 +0530

    Time: 27 ms (25.18%) | Memory: 55.3 MB (79.91%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,29 +1,32 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<List<Integer>> revAdj = new ArrayList<>();
         int n = graph.length;
-        
+        List<List<Integer>> adj = new ArrayList<>();
         int[] indegree = new int[n];
-        for(int i =0;i<n;i++){
-            revAdj.add(new ArrayList<>());
+
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());            
         }
         for(int i =0;i<n;i++){
             for(int it : graph[i]){
-                revAdj.get(it).add(i);
+                adj.get(it).add(i);
                 indegree[i]++;
             }
         }
-        List<Integer> ans = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         for(int i =0;i<n;i++){
-            if(indegree[i] == 0) q.add(i);
+            if(indegree[i] == 0){
+                q.add(i);
+            }
         }
+
+        List<Integer> ans = new ArrayList<>();
+
         while(!q.isEmpty()){
             int u = q.poll();
             ans.add(u);
-            for(int v: revAdj.get(u)){
-                indegree[v]--;
-                if(indegree[v] == 0){
+            for(int v : adj.get(u)){
+                if(--indegree[v] == 0){
                     q.add(v);
                 }
             }
