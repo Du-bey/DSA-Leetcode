3302e29168a0011cf4728ee72c71b781d2fe7087
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-14 20:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (21.81%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,40 +1,35 @@
 class Solution {
-    public int splitArray(int[] arr, int k) {
-        int sum = 0;
-        int n = arr.length;
-        if(k > n) return -1;
-        int max = Integer.MIN_VALUE;
-        for(int i =0;i<n;i++){
-            max = Math.max(arr[i], max);
-            sum += arr[i];
+    public int splitArray(int[] nums, int k) {
+        int l = 0;
+        int h = 0;
+        for(int x : nums){
+            l = Math.max(l, x);
+            h += x;
         }
-        int l = max;
-        int h = sum;
+
         while(l <= h){
-            int m = (l+h)/2;
-            int noOfSt = check(arr, m, n);
-            if(noOfSt > k){
-                l = m + 1;
+            int m = (l + h)/2;
+            if(isPossible(m, nums, k)){
+                h = m - 1;
             }
             else{
-                h = m - 1;
+                l = m + 1;
             }
         }
         return l;
     }
-    public static int check(int[] arr, int m, int n){
-        int st = 1;
-        int pg = 0;
-        for(int i =0;i<n;i++){
-            int no = arr[i];
-            if(pg + no <= m){
-                pg += no;
-            }
-            else{
-                st++;
-                pg = no;
+    
+    public boolean isPossible(int m, int[] nums, int k){
+        int div = 0;
+        int sum = 0;
+        for(int x : nums){
+            sum += x;
+            if(sum > m){
+                sum = x;
+                div++;
             }
         }
-        return st;
+        if(sum > 0) div++;
+        return div <= k;
     }
 }
