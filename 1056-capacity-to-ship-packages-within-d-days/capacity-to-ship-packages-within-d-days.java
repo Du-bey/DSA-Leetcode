e8928a2eaddbfa71b23c36503213837df8987243
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-14 11:25:50 +0530

    Time: 11 ms (72.61%) | Memory: 46.8 MB (93.73%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,36 +1,37 @@
 class Solution {
-    public int shipWithinDays(int[] w, int days) {
-        int n = w.length;
+    public int shipWithinDays(int[] arr, int days) {
         int l = 0;
-        int h =0;
-        for(int x : w){
-            h+= x;
+        int h = 0;
+        for(int x : arr){
             l = Math.max(l, x);
+            h += x;
         }
+
         int ans = 0;
         while(l <= h){
-            int m = (l+h)/2;
-            if(check(m, w, days)){
+            int m = (l+h) / 2;
+            if(isPossible(m, arr, days)){
                 h = m - 1;
                 ans = m;
             }
-            else{
+            else {
                 l = m + 1;
             }
         }
-        return l;
+        return ans;
     }
 
-    public boolean check(int m, int[] w, int days){
-        int d = 1;
-        int wt = 0;
-        for(int x : w){
-            wt += x;
-            if(wt > m){
-                wt = x;
+    public boolean isPossible(int m, int[] arr, int days){
+        int d = 0;
+        int cap = 0;
+        for(int x : arr){
+            cap += x;
+            if(cap > m){
+                cap = x;
                 d++;
             }
         }
+        if(cap > 0) d++;
         return d <= days;
     }
 }
