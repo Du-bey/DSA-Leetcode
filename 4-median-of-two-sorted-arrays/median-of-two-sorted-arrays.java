d110e13bf16d278a50b77639ad803206a00f25c6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-14 23:15:17 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (20.83%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,25 +1,20 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
-        int left = (n1+n2+1)/2;
-        int l =0;
-        int h =n1;
-        int n = n1+n2;
-        while(l <=h){
-            int mid1 = (l+h)/2;
-            int mid2 = left - mid1;
-            int l1 = Integer.MIN_VALUE;
-            int l2 = Integer.MIN_VALUE;
-            int r1 = Integer.MAX_VALUE;
-            int r2 = Integer.MAX_VALUE;
-            if(mid1 < n1) r1 = nums1[mid1];
-            if(mid2 < n2) r2 = nums2[mid2];
-            if(mid1 - 1 >=0) l1 = nums1[mid1-1];
-            if(mid2 - 1 >= 0) l2 = nums2[mid2-1];
-            if((l1 <=r2) && (l2 <=r1)){
-                if(n %2 == 0){
+        int n = nums1.length, m = nums2.length;
+        if(n > m) return findMedianSortedArrays(nums2, nums1);
+        int tSize = n + m, left = (n+m+1)/2;
+        boolean isEven = tSize % 2 == 0;
+        int l = 0, h = n;
+        while(l <= h){
+            int mid = (l+h) / 2;
+            int mid2 = left - mid;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if(mid > 0) l1 = nums1[mid-1];
+            if(mid2 > 0) l2 = nums2[mid2-1];
+            if(mid < n) r1 = nums1[mid];
+            if(mid2 < m) r2 = nums2[mid2];
+            if((l1 <= r2) && (l2 <= r1)){
+                if(isEven){
                     return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
                 }
                 else{
@@ -27,10 +22,10 @@ class Solution {
                 }
             }
             else if(l1 < r2){
-                l = mid1 +1;
+                l = mid + 1;
             }
             else{
-                h = mid1-1;
+                h = mid - 1;
             }
         }
         return -1;
