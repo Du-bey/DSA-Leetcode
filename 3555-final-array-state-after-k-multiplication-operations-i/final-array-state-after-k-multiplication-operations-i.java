a4080fb912787d992315dba9ab2b4e73ffd467ee
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-16 22:23:58 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (89.91%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.java
@@ -1,26 +1,19 @@
 class Solution {
-    public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (a[0] == b[0]) {
-                return Integer.compare(a[1], b[1]); 
+    public int[] getFinalState(int[] arr, int k, int multiplier) {
+        int ans[]=new int[arr.length];
+        while(k>0)
+        {
+            int x=0;
+            for(int i=1;i<arr.length;i++)
+            {
+                if(arr[i]<arr[x])
+                {
+                    x=i;
+                }
             }
-            return Integer.compare(a[0], b[0]);
-        });
-
-        for (int i = 0; i < nums.length; i++) {
-            pq.add(new int[] { nums[i], i });
+            arr[x]=arr[x]*multiplier;
+            k--;
         }
-
-        while (k-- > 0) {
-            int[] smallest = pq.poll();
-            smallest[0] *= multiplier;
-            pq.add(smallest);
-        }
-
-        while (!pq.isEmpty()) {
-            int[] entry = pq.poll();
-            nums[entry[1]] = entry[0];
-        }
-        return nums;
+        return arr;
     }
 }
