d03f6c56342fad9f692d5b5a8fafdd2f77f49813
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-22 00:25:09 +0530

    Time: 11 ms (24.39%) | Memory: 48.7 MB (43.20%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,30 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        int lvl = 0;
         Queue<TreeNode> q = new LinkedList<>();
-
         q.add(root);
-        while (!q.isEmpty()) {
+        boolean odd = false;
+        while(!q.isEmpty()){
             int size = q.size();
-            List<Integer> nodes = new ArrayList<>();
-            Queue<TreeNode> temp = new LinkedList<>(q);
-            for (int i = 0; i < size; i++) {
-                TreeNode it = q.poll();
-                if (it.left != null) q.add(it.left);
-                if (it.right != null) q.add(it.right);
-                nodes.add(it.val);
+            List<TreeNode> list = new ArrayList<>();
+            for(int i =0;i<size;i++){
+                TreeNode u = q.poll();
+                list.add(u);
+                if(u.left != null) q.add(u.left);
+                if(u.right != null) q.add(u.right);
             }
-            if (lvl % 2 != 0) {
-                Collections.reverse(nodes);
-                int i = 0;
-                while (size-- > 0) {
-                    TreeNode it = temp.poll();
-                    it.val = nodes.get(i);
-                    i++;
-                }
-            }
-            lvl++;
+            if(odd) reverse(list);
+            odd = !odd;
         }
         return root;
+    }
+
+    public void reverse(List<TreeNode> list){
+        int n = list.size();
+        int i = 0;
+        int j = n-1;
+        while(i <= j){
+            int temp = list.get(i).val;
+            list.get(i).val = list.get(j).val;
+            list.get(j).val = temp;
+            
+            i++;
+            j--;
+        }
     }
 }
