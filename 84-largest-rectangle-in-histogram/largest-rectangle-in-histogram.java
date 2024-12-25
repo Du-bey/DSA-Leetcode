3ea6d9586e8d3353a39fc3a35c8bb656c3a25f03
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-25 12:00:15 +0530

    Time: 64 ms (65.81%) | Memory: 57.7 MB (38.71%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,37 +1,24 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
         int n = arr.length;
-        int[] nse = nse(arr, n);
-        int[] pse = pse(arr, n);
         int ans = 0;
+        Stack<Integer> st = new Stack<>();
         for(int i =0;i<n;i++){
-            int res = (nse[i] - 1 - pse[i]);
-            res *= arr[i];
-            ans = Math.max(res, ans);
-        }
-        return ans;
-    }
-
-    public int[] nse(int[] arr,int n){
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for(int i = n - 1;i>=0;i--){
-            int num = arr[i];
-            while(!st.isEmpty() && num <= arr[st.peek()]) st.pop();
-            ans[i] = st.isEmpty() ? n : st.peek();
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                int ind = st.pop();
+                int prev = st.isEmpty() ? -1 : st.peek();
+                int area = arr[ind] * (i - prev - 1);
+                ans = Math.max(ans, area);
+            }
             st.push(i);
         }
-        return ans;
-    }
 
-    public int[] pse(int[] arr, int n){
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for(int i =0; i<n; i++){
-            int num = arr[i];
-            while(!st.isEmpty() && num < arr[st.peek()]) st.pop();
-            ans[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
+        while(!st.isEmpty()){
+            int ind = st.pop();
+            int next = n;
+            int prev = st.isEmpty() ? -1 : st.peek();
+            int area = arr[ind] * (n - prev - 1);
+            ans = Math.max(ans, area);
         }
         return ans;
     }
