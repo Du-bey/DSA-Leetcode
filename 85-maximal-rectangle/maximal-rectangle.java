e0355fb976fe4c8eac6637ba42e31262a44d3080
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-25 12:27:58 +0530

    Time: 16 ms (45.93%) | Memory: 46 MB (73.86%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -2,68 +2,46 @@ class Solution {
     public int maximalRectangle(char[][] matrix) {
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] prefix = new int[n][m];
-
-        for(int i =0;i<n;i++){
-            for(int j=0;j<m;j++){
-                prefix[i][j] = 0;
-            }
-        }
+        int[][] pre = new int[n][m];
 
         for(int j =0;j<m;j++){
             int sum = 0;
-            for(int i=0;i<n;i++){
+            for(int i =0;i<n;i++){
                 int number = matrix[i][j] == '1' ? 1 : 0;
                 sum += number;
                 if(matrix[i][j] == '0'){ 
                     sum = 0;
                 }
-                prefix[i][j] = sum;
+                pre[i][j] = sum;
             }
         }
-
+        int ans = 0;
         for(int i =0;i<n;i++){
-            for(int j=0;j<m;j++){
-                 System.out.print(prefix[i][j] + " ");
-            }
-            System.out.println();
-        }
-        int ans = -1;
-        for(int i =0;i<n;i++){
-            int area = largestRectangleArea(prefix[i]);
-            ans = Math.max(ans, area);
+            ans = Math.max(ans, largestRectangleArea(pre[i]));
         }
         return ans;
     }
 
-    public static int largestRectangleArea(int[] h) {
-        int n = h.length;
-        if(n == 1)
-        return h[0];
-        int ans = -1;
+    public int largestRectangleArea(int[] arr) {
+        int n = arr.length;
+        int ans = 0;
         Stack<Integer> st = new Stack<>();
         for(int i =0;i<n;i++){
-            while(!st.isEmpty() && h[i] < h[st.peek()]){
-                int no = h[st.pop()];
-                int next = i;
-                int prev = -1;
-                if(!st.isEmpty()){
-                    prev = st.peek();
-                }
-                int w = next - prev - 1;
-                int a = w * no;
-                ans = Math.max(ans, a);
+            while(!st.isEmpty() && arr[st.peek()] > arr[i]){
+                int ind = st.pop();
+                int prev = st.isEmpty() ? -1 : st.peek();
+                int area = arr[ind] * (i - prev - 1);
+                ans = Math.max(ans, area);
             }
             st.push(i);
         }
 
         while(!st.isEmpty()){
-            int no = h[st.pop()];
+            int ind = st.pop();
             int next = n;
             int prev = st.isEmpty() ? -1 : st.peek();
-            int w = next - prev - 1;
-            int a = w * no;
-            ans = Math.max(ans, a);
+            int area = arr[ind] * (n - prev - 1);
+            ans = Math.max(ans, area);
         }
         return ans;
     }
