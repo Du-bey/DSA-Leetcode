0b20fa289429a2dc6771680a39cbe08b71e7efc7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-28 18:51:22 +0530

    Time: 3 ms (80.79%) | Memory: 42.8 MB (24.49%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -15,33 +15,32 @@
  */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> h = new TreeMap<>();
         Queue<Triplet> q = new LinkedList<>();
         q.add(new Triplet(root, 0, 0));
 
         while(!q.isEmpty()){
-            Triplet t = q.peek();
+            Triplet t = q.poll();
             TreeNode node = t.val;
             int ver = t.ver;
-            int val = t.lev;
-            q.poll(); 
-            map.putIfAbsent(ver, new TreeMap<>());
-            map.get(ver).putIfAbsent(val, new PriorityQueue<>());
-            map.get(ver).get(val).offer(node.val);
-            if(node.left != null) q.add(new Triplet(node.left, ver - 1, val +1));
-            if(node.right != null) q.add(new Triplet(node.right, ver + 1, val +1));
+            int lev = t.lev;
+            h.putIfAbsent(ver, new TreeMap<>());
+            h.get(ver).putIfAbsent(lev, new PriorityQueue<>());
+            h.get(ver).get(lev).add(node.val);
+            if(node.left != null) q.add(new Triplet(node.left, ver - 1, lev + 1));
+            if(node.right != null) q.add(new Triplet(node.right, ver + 1, lev + 1));
         }
 
-        List<List<Integer>> list = new ArrayList<>();
-        for(TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()){
-            list.add(new ArrayList<>());
+        List<List<Integer>> ans = new ArrayList<>();
+        for(TreeMap<Integer, PriorityQueue<Integer>> ys : h.values()){
+            ans.add(new ArrayList<>());
             for(PriorityQueue<Integer> nodes : ys.values()){
                 while(!nodes.isEmpty()){
-                    list.get(list.size()-1).add(nodes.poll());
+                    ans.get(ans.size() - 1).add(nodes.poll());
                 }
             }
         }
-        return list;
+        return ans;
     }
 }
 
