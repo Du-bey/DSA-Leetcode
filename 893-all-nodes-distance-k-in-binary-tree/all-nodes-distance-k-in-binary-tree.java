ba220df74342c4155198a9fbfa5eb1d87d0bcf59
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-29 18:50:39 +0530

    Time: 12 ms (88.24%) | Memory: 43.1 MB (25.84%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -1,64 +1,50 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        Map<TreeNode, TreeNode> p = new HashMap<>();
-        markParent(root, p);
-        Map<TreeNode, Boolean> vis = new HashMap<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(target);
-        vis.put(target, true);
-        int lev = 0;
-        while(!q.isEmpty()){
-            int size = q.size();
-            if(lev == k) break;
-            lev++;
-            for(int i =0;i<size;i++){
-                TreeNode node = q.poll();
-                if(node.left != null && vis.get(node.left) == null){
-                    q.add(node.left);
-                    vis.put(node.left, true);
-                }
-                if(node.right != null && vis.get(node.right) == null){
-                    q.add(node.right);
-                    vis.put(node.right, true);
-                }
-                TreeNode pnode = p.get(node);
-                if(pnode != null && vis.get(pnode) == null){
-                    q.add(pnode);
-                    vis.put(pnode, true);
-                }
-            }
-        }
-        List<Integer> ans = new ArrayList<>();
-        while(!q.isEmpty()){
-            TreeNode node = q.poll();
-            ans.add(node.val);
-        }
-        return ans;
-    }
-
-    public void markParent(TreeNode root, Map<TreeNode, TreeNode> p){
-        if(root == null) return;
+        HashMap<TreeNode, TreeNode> h = new HashMap<>();
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         while(!q.isEmpty()){
             TreeNode node = q.poll();
             if(node.left != null){
-                p.put(node.left, node);
+                h.put(node.left, node);
                 q.add(node.left);
             }
             if(node.right != null){
-                p.put(node.right, node);
+                h.put(node.right, node);
                 q.add(node.right);
             }
         }
+        q.add(target);
+
+        List<Integer> ans = new ArrayList<>();
+        int dis = 0;
+        HashSet<TreeNode> vis = new HashSet<>();
+        vis.add(target);
+        while(!q.isEmpty()){
+            int size = q.size();
+            if(dis == k) break;
+            dis++;
+            for(int i =0;i<size;i++){
+                TreeNode node = q.poll();
+                TreeNode par = h.get(node);
+
+                if(node.left != null && !vis.contains(node.left)){
+                    q.add(node.left);
+                    vis.add(node.left);
+                }
+                if(node.right != null && !vis.contains(node.right)){
+                    q.add(node.right);
+                    vis.add(node.right);
+                }
+                if(par != null && !vis.contains(par)){
+                    q.add(par);
+                    vis.add(par);
+                }
+            }
+        }
+        while(!q.isEmpty()){
+            ans.add(q.poll().val);
+        }
+        return ans;
     }
 }
