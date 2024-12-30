d9c6b561c68a9fb1badd7dcc9ade6e2ec046e260
Du-bey <54775278+Du-bey@users.noreply.github.com>
2024-12-31 02:58:34 +0530

    Time: 1 ms (99.64%) | Memory: 44.2 MB (59.12%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,16 +1,22 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> t) {
-        int n = t.size();
-        if(n == 1) return t.get(0).get(0);
-        int [][] dp = new int[n+1][n+1];
-        for(int i =n-1;i>=0;i--){
-            for(int j =0;j<=i;j++){
-                int dr = dp[i+1][j+1];
-                int d = dp[i+1][j];
-                int cost = t.get(i).get(j) + Math.min(dr, d);
-                dp[i][j] = cost;
-            }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int[] r : dp){
+            Arrays.fill(r, -1);
         }
-        return dp[0][0];
+        return f(0, 0, n, triangle, dp);
+    }
+
+    public int f(int i, int ind, int n, List<List<Integer>> t, int[][] dp){
+        if(i == n) return 0;
+        if(dp[i][ind] != -1) return dp[i][ind];
+
+        int ans = Integer.MAX_VALUE;
+        int down = f(i+1, ind, n, t, dp);
+        int right = f(i+1, ind + 1, n, t, dp);
+        int min = Math.min(down, right);
+        ans = Math.min(ans, min);
+        return dp[i][ind] = ans + t.get(i).get(ind);
     }
 }
