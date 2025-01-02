106bf73da175daada5aa146472e4a41b509e901e
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-03 00:44:02 +0530

    Time: 17 ms (56.54%) | Memory: 48.6 MB (22.75%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,37 +2,33 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        boolean[][] dp = new boolean[n+1][m+1];
-        dp[0][0] = true;
-        for(int i =1;i<=n;i++){
-            dp[i][0] = false;
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
         }
-        for(int j =1;j<=m;j++){
-            boolean flag = true;
-            for(int j1 =1;j1 <=j;j1++){
-                if(p.charAt(j1-1) != '*') {
-                    flag = false;
-                    break;
-                }
-            }
-            dp[0][j] = flag;
-        }
-
-        for(int i =1;i<=n;i++){
-            for(int j =1;j<=m;j++){
-                if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?'){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else if(p.charAt(j-1) == '*'){
-                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
-                }
-                else{
-                    dp[i][j] = false;
-                }
-            }
-        }
-        return dp[n][m];
+        return f(n-1, m-1, s, p, dp);
     }
 
-    
+    public boolean f(int i, int j, String s, String p, int[][] dp){
+        if(i < 0 && j <0) return true;
+        if(i >= 0 && j <0) return false;
+        if(i < 0 && j >= 0){
+            while(j >= 0){
+                if(p.charAt(j) != '*') return false;
+                j--;
+            }
+            return true;
+        }
+        if(dp[i][j] != -1) return dp[i][j] == 0 ? false : true;
+
+        boolean ans = false;
+        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+            ans = f(i-1, j-1, s, p, dp);
+        }
+        else if(p.charAt(j) == '*'){
+            ans = (f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp));
+        }
+        dp[i][j] = ans ? 1 : 0;
+        return ans;
+    }
 }
