d350afd487d54fcff0f49374b52d11bb110da8c1
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-04 02:11:07 +0530

    Time: 29 ms (20.41%) | Memory: 45.4 MB (70.61%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,33 +1,35 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
         int[] dr = {0, -1, -1, -1, 0, 1, 1, 1};
         int[] dc = {-1, -1, 0, 1, 1, 1, 0, -1};
 
-        int n = grid.length;
+        PriorityQueue<tuple> pq = new PriorityQueue<>((a, b) -> a.first - b.first);
+        pq.add(new tuple(1, 0, 0));
         int[][] dis = new int[n][n];
         for(int i = 0;i<n;i++) {
             for(int j =0;j<n;j++) {
-                dis[i][j] = Integer.MAX_VALUE;
+                dis[i][j] = (int) 1e9;
             }
         }
         dis[0][0] = 1;
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
-        grid[0][0] = -1;
-        Queue<tuple> pq = new LinkedList<>();
-        pq.add(new tuple(1, 0, 0));
+        //grid[0][0] = -1;
         while(!pq.isEmpty()){
-            int d = pq.peek().first;
-            int r = pq.peek().second;
-            int c = pq.peek().third;
-            pq.remove();
+            tuple t = pq.poll();
+            int d = t.first;
+            int r = t.second;
+            int c = t.third;
             if(r == n-1 && c == n-1) return d;
             for(int i =0;i<8;i++){
-                int newr = r+dr[i];
-                int newc = c+dc[i];
+                int newr = r + dr[i];
+                int newc = c + dc[i];
                 if(newr >=0 && newc >=0 && newr <n && newc <n && grid[newr][newc] == 0){
                     if(dis[newr][newc] > d+1){
-                        pq.add(new tuple(d+1, newr, newc));
-                        grid[newr][newc] = -1;
+                        dis[newr][newc] = d + 1;
+                        pq.add(new tuple(dis[newr][newc], newr, newc));
+                        //grid[newr][newc] = -1;
                     }
                 }
             }
