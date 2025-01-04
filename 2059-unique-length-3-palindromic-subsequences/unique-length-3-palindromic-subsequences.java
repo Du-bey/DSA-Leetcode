48fcd93175ce8f776fe229736718080432093d03
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-04 18:15:17 +0530

    Time: 174 ms (71.20%) | Memory: 45.6 MB (47.91%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -3,19 +3,23 @@ class Solution {
         HashSet<Character> set = new HashSet<>();
         int n = s.length();
         int ans = 0;
+        int[] pre = new int[26];
+        int[] post = new int[26];
+        Arrays.fill(pre, -1);
+        Arrays.fill(post, -1);
+        
         for(int i =0;i<n;i++){
             char c = s.charAt(i);
+            int ascii = c - 'a';
+            if(pre[ascii] == -1) pre[ascii] = i;
+            post[ascii] = i;
             set.add(c);
         }
+
         for(char c : set){
-            int left = -1;
-            int right = -1;
-            for(int i =0;i<n;i++){
-                if(s.charAt(i) == c){
-                    if(left == -1) left = i;
-                    right = i;
-                }
-            }
+            int left = pre[c - 'a'];
+            int right = post[c - 'a'];
+            
             HashSet<Character> st = new HashSet<>();
             for(int mid = left + 1; mid < right;mid++){
                 st.add(s.charAt(mid));
