f0e62fb9cef4f852e09c16aa89f1d7f3ea8f07fd
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-05 03:51:22 +0530

    Time: 19 ms (24.49%) | Memory: 67.6 MB (34.43%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,18 +1,20 @@
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        int extra = 0;
         DisjointSet ds = new DisjointSet(n);
-        for(int [] r : connections){
-            if(!ds.unionBySize(r[0], r[1])){
-                extra++;
+        int extra = 0;
+        for(int[] r : connections){
+            int a = r[0];
+            int b = r[1];
+            if(!ds.unionBySize(a, b)) extra++;
+        }
+        int reqd = 0;
+        for(int i =0;i<n;i++){
+            if(ds.findUPar(i) == i){
+                reqd++;
             }
         }
-        int island = 0;
-        for(int i =0;i<n;i++){
-            if(ds.parent.get(i) == i) island++;
-        }
-        if(island - 1 > extra) return -1;
-        return island - 1;
+        reqd--;
+        return (extra >= reqd) ? reqd : -1;
     }
 }
 
@@ -20,29 +22,28 @@ public class DisjointSet {
     List<Integer> rank = new ArrayList<>();
     List<Integer> parent = new ArrayList<>();
     List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n) {
-        for(int i =0;i<=n;i++){
+
+    public DisjointSet(int n){
+        for(int i = 0;i<=n;i++){
+            size.add(1);
             rank.add(0);
             parent.add(i);
-            size.add(1);
         }
     }
 
     public int findUPar(int node){
-        var pNode = parent.get(node);
-        if(node == pNode){
-            return node;
-        }
-        int ulp = findUPar(pNode);
-        parent.set(node, ulp);
-        return ulp;
+        int par = parent.get(node);
+        if(par == node) return node;
+        int upar = findUPar(par);
+        parent.set(node, upar);
+        return upar;
     }
 
-    public void unionByRank(int u, int v){
+    public boolean unionByRank(int u, int v){
         int up = findUPar(u);
         int vp = findUPar(v);
 
-        if(up == vp) return;
+        if(up == vp) return false;
         int ru = rank.get(u);
         int rv = rank.get(v);
         if(rv > ru){
@@ -57,20 +58,19 @@ public class DisjointSet {
             upRank++;
             rank.set(up, upRank);
         }
+        return true;
     }
 
     public boolean unionBySize(int u, int v){
         int up = findUPar(u);
         int vp = findUPar(v);
-
         if(up == vp) return false;
-        int ru = size.get(u);
-        int rv = size.get(v);
-        if(rv > ru){
+        int su = size.get(u);
+        int sv = size.get(v);
+        if(sv > su){
             parent.set(up, vp);
             size.set(vp, size.get(up) + size.get(vp));
         }
-
         else{
             parent.set(vp, up);
             size.set(up, size.get(up) + size.get(vp));
