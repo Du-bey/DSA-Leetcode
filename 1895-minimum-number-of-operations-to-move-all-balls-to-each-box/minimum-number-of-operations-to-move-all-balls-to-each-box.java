cb60d24aa28a2c9b3a93b90251bc85839c26b93a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-12 20:26:29 +0530

    Time: 4 ms (62.96%) | Memory: 44.9 MB (30.73%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,17 +1,28 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
-        int[] ans = new int[n];
-        for(int i =0;i<n;i++){
-            char c = boxes.charAt(i);
-            int res = 0;
-            for(int j = 0;j<n;j++){
-                char ch = boxes.charAt(j);
-                if(i == j) continue;
-                if(ch == '1') res += Math.abs(i - j);
-            }
-            ans[i] = res;
+        int[] answer = new int[n];
+
+        int cumValue = 0;
+        int cumValueSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = cumValueSum;
+
+            cumValue += boxes.charAt(i) == '0' ? 0 : 1;
+            cumValueSum += cumValue;
         }
-        return ans;
+
+        cumValue = 0;
+        cumValueSum = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] += cumValueSum;
+
+            cumValue += boxes.charAt(i) == '0' ? 0 : 1;
+            cumValueSum += cumValue;
+        }
+
+        return answer;
     }
 }
