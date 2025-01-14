1f286f6714c96d72a01f70e5ee361c5346c3e134
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-14 13:26:19 +0530

    Time: 882 ms (29.23%) | Memory: 184.6 MB (47.41%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -1,70 +1,37 @@
 class Solution {
     public int findMaximumXOR(int[] nums) {
-        Trie trie = new Trie();
-        for (int it : nums) {
-            trie.insert(it);
+        Node root = new Node();
+        for(int n : nums){
+            Node node = root;
+            for(int i = 31;i>=0;i--){
+                int bit = (n >> i) & 1;
+                if(node.links[bit] == null){
+                    node.links[bit] = new Node();
+                }
+                node = node.links[bit];
+            }
         }
 
-        int maxi = 0;
-        for (int it : nums) {
-            maxi = Math.max(maxi, trie.getMax(it));
+        int ans = 0;
+        for(int n : nums){
+            Node node = root;
+            int max = 0;
+            for(int i = 31;i>=0;i--){
+                int bit = (n >> i) & 1;
+                if(node.links[1 - bit] != null){
+                    max = max | (1 << i);
+                    node = node.links[1 - bit];
+                }
+                else{
+                    node = node.links[bit];
+                }
+            }
+            ans = Math.max(ans, max);
         }
-        return maxi;
+        return ans;
     }
 }
 
-class Node {
-    Node[] links;
-    Node() {
-        links = new Node[2];
-    }
-
-    boolean containsKey(int bit) {
-        return links[bit] != null;
-    }
-
-    Node get(int bit) {
-        return links[bit];
-    }
-
-    void put(int bit, Node node) {
-        links[bit] = node;
-    }
-}
-
-class Trie {
-    Node root;
-
-    Trie() {
-        root = new Node();
-    }
-
-    void insert(int num) {
-        Node node = root;
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-
-            if (!node.containsKey(bit)) {
-                node.put(bit, new Node());
-            }
-            node = node.get(bit);
-        }
-    }
-
-    int getMax(int num) {
-        Node node = root;
-
-        int maxNum = 0;
-
-        for (int i = 31; i >= 0; i--) {
-            int bit = (num >> i) & 1;
-            if (node.containsKey(1 - bit)) {
-                maxNum |= (1 << i);
-                node = node.get(1 - bit);
-            } else {
-                node = node.get(bit);
-            }
-        }
-        return maxNum;
-    }
+class Node{
+    Node[] links = new Node[2];
 }
