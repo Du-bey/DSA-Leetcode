4a6ede2baf852fb1687975b311aee19eb6845765
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-17 11:40:42 +0530

    Time: 2 ms (100.00%) | Memory: 62.1 MB (68.82%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,13 +1,9 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        int n = derived.length;
-        int isFirstAndLastSame = derived[n-1];
-        int last = 0;
-        for(int i =0;i<n-1;i++){
-            if(derived[i] == 1){
-                last = 1 - last;
-            }
+        int xr = 0;
+        for(int x : derived){
+            xr ^= x;
         }
-        return isFirstAndLastSame == last;
+        return xr == 0;
     }
 }
