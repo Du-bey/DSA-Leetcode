1bfff4409687463d1cf9c22d87f4781349ffa9bd
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-18 19:21:47 +0530

    Time: 2 ms (71.47%) | Memory: 42.5 MB (59.25%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -6,17 +6,16 @@ class Solution {
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
 
-        Queue<Triplet> q = new LinkedList<>();
         boolean[][] vis = new boolean[n][m];
-
+        Queue<Triplet> q = new LinkedList<>();
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(grid[i][j] == 2){
                     q.add(new Triplet(0, i, j));
+                    vis[i][j] = true;
                 }
             }
         }
-
         int ans = 0;
         while(!q.isEmpty()){
             Triplet trip = q.poll();
@@ -25,24 +24,22 @@ class Solution {
             int c = trip.c;
 
             ans = Math.max(ans, t);
-
             for(int i =0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr >=0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1){
-                    q.add(new Triplet(t + 1, nr, nc));
-                    grid[nr][nc] = 2;
+                if(nr >=0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 1 && !vis[nr][nc]){
+                    q.add(new Triplet(t+1, nr, nc));
+                    vis[nr][nc] = true;
                 }
             }
         }
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == 1){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     return -1;
                 }
             }
         }
-
         return ans;
     }
 }
