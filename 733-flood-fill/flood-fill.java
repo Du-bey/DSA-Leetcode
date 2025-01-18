42a647f24f039196386f137ba25c06d905388ee9
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-18 19:59:55 +0530

    Time: 1 ms (44.31%) | Memory: 45.2 MB (16.76%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
-
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr, sc));
 
+        int n = image.length;
+        int m = image[0].length;
         int oc = image[sr][sc];
         if(oc == color) return image;
+        
         image[sr][sc] = color;
 
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr, sc));
         while(!q.isEmpty()){
             Pair p = q.poll();
             int r = p.a;
             int c = p.b;
+
             for(int i =0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr >=0 && nr < n && nc >= 0 && nc < m && image[nr][nc] == oc){
+                if(nr >=0 && nc >= 0 && nr < n && nc < m && image[nr][nc] == oc){
                     q.add(new Pair(nr, nc));
                     image[nr][nc] = color;
                 }
@@ -28,7 +29,6 @@ class Solution {
         return image;
     }
 }
-
 public class Pair {
     int a;
     int b;
