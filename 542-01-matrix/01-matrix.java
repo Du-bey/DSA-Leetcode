7745c4b03ebd427b36bfdfa3372694838fb62996
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-18 15:58:24 +0530

    Time: 15 ms (39.27%) | Memory: 47.7 MB (29.27%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,11 +1,9 @@
 class Solution {
-    public int[][] updateMatrix(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
+    public int[][] updateMatrix(int[][] mat) {
         int[] dr = {0, 1, 0, -1};
         int[] dc = {1, 0, -1, 0};
-
+        int n = mat.length;
+        int m = mat[0].length;
         Queue<Triplet> q = new LinkedList<>();
         boolean[][] vis = new boolean[n][m];
 
@@ -16,27 +14,26 @@ class Solution {
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(grid[i][j] == 0){
-                    ans[i][j] = 0;
+                if(mat[i][j] == 0){
                     q.add(new Triplet(0, i, j));
+                    ans[i][j] = 0;
                     vis[i][j] = true;
                 }
             }
         }
-
         while(!q.isEmpty()){
-            Triplet trip = q.poll();
-            int t = trip.a;
-            int r = trip.b;
-            int c = trip.c;
+            Triplet t = q.poll();
+            int dis = t.first;
+            int r = t.second;
+            int c = t.third;
 
             for(int i =0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr >=0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && !vis[nr][nc]){
-                    q.add(new Triplet(t + 1, nr, nc));
-                    ans[nr][nc] = t+1;
+                if(nr >=0 && nc >= 0 && nr < n && nc < m && mat[nr][nc] == 1 && !vis[nr][nc]){
+                    q.add(new Triplet(dis + 1, nr, nc));
                     vis[nr][nc] = true;
+                    ans[nr][nc] = dis + 1;
                 }
             }
         }
@@ -45,13 +42,13 @@ class Solution {
 }
 
 public class Triplet {
-    int a;
-    int b;
-    int c;
+    int first;
+    int second;
+    int third;
 
-    public Triplet(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public Triplet(int first, int second, int third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
     }
 }
