7d0ac7ae2003b773722044d9c72d33c2b4e48dc6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-19 14:10:52 +0530

    Time: 2 ms (53.67%) | Memory: 41.2 MB (35.51%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.java
+++ 1304-longest-happy-string/longest-happy-string.java
@@ -1,16 +1,43 @@
 class Solution {
-    String generate(int a, int b, int c, String aa, String bb, String cc) {
-    if (a < b)
-        return generate(b, a, c, bb, aa, cc);
-    if (b < c)
-        return generate(a, c, b, aa, cc, bb);
-    if (b == 0)
-        return aa.repeat(Math.min(2, a));
-    int use_a = Math.min(2, a), use_b = a - use_a >= b ? 1 : 0; 
-    return aa.repeat(use_a) + bb.repeat(use_b) +
-        generate(a - use_a, b - use_b, c, aa, bb, cc);    
-    }
     public String longestDiverseString(int a, int b, int c) {
-        return generate(a, b, c, "a", "b", "c");
+        PriorityQueue<Pair> pq = new PriorityQueue<>((s, t) -> t.a - s.a); 
+
+        if(a > 0) pq.add(new Pair(a, 'a'));
+        if(b > 0) pq.add(new Pair(b, 'b'));
+        if(c > 0) pq.add(new Pair(c, 'c'));
+
+        StringBuilder sb = new StringBuilder();
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
+            int len = p.a;
+            char ch = p.b;
+            if(sb.length() >= 2 && sb.charAt(sb.length() - 1) == ch && sb.charAt(sb.length() - 2) == ch){
+                if(pq.isEmpty()) break;
+                Pair p2 = pq.poll();
+                int len2 = p2.a;
+                char c2 = p2.b;
+                sb.append(c2);
+                len2--;
+                if(len2 > 0){
+                    pq.add(new Pair(len2, c2));
+                }
+                pq.add(p);
+            }
+            else{
+                sb.append(ch);
+                len--;
+                if(len > 0) pq.add(new Pair(len, ch));
+            }
+        }
+        return sb.toString();
+    }
+}
+
+class Pair{
+    int a;
+    char b;
+    public Pair(int a , char b){
+        this.a = a;
+        this.b = b;
     }
 }
