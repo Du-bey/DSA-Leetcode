9d3e923c385d4dc6affbe10d467d48880e565f9b
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-21 01:09:58 +0530

    Time: 23 ms (71.63%) | Memory: 65.8 MB (64.90%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,45 +1,30 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
         HashMap<Integer, Pair> h = new HashMap<>();
-        HashMap<Integer, Integer> row = new HashMap<>();
-        HashMap<Integer, Integer> col = new HashMap<>();
+        int[] row = new int[n];
+        int[] col = new int[m];
 
         for(int i =0;i<mat.length;i++){
-            row.put(i, 0);
             for(int j =0;j<mat[0].length;j++){
-                h.put(mat[i][j] , new Pair(i, j));
-                int val = row.get(i);
-                val += mat[i][j];
-                row.put(i , val);
-            }
-        }
-        
-        for(int j =0;j<mat[0].length;j++){
-            col.put(j, 0);
-            for(int i =0;i<mat.length;i++){
-                int val = col.get(j);
-                val += mat[i][j];
-                col.put(j , val);
+                int x = mat[i][j];
+                h.put(x , new Pair(i, j));
+                
+                row[i] += x;
+                col[j] += x;
             }
         }
 
         for(int i =0;i<arr.length;i++){
             int x = arr[i];
             Pair p = h.get(x);
-
             int r = p.r;
             int c = p.c;
 
-            int rsum = row.get(r);
-            rsum -= x;
-            if(rsum == 0) return i;
-            row.put(r, rsum);
-
-            int csum = col.get(c);
-            csum -= x;
-            if(csum == 0) return i;
-            col.put(c, csum);
-
+            row[r] -= x;
+            col[c] -= x;
+            if(row[r] == 0 || col[c] == 0) return i;
         }
         return -1;
     }
