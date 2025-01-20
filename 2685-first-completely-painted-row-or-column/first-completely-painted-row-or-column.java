bb1aadf4c207bf8b2d6514f6e6450436bbc7ce87
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-21 01:04:23 +0530

    Time: 71 ms (8.46%) | Memory: 76.3 MB (10.45%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.java
@@ -1,21 +1,19 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
         HashMap<Integer, Pair> h = new HashMap<>();
-        for(int i =0;i<mat.length;i++){
-            for(int j =0;j<mat[0].length;j++){
-                h.put(mat[i][j] , new Pair(i, j));
-            }
-        }
         HashMap<Integer, Integer> row = new HashMap<>();
         HashMap<Integer, Integer> col = new HashMap<>();
+
         for(int i =0;i<mat.length;i++){
             row.put(i, 0);
             for(int j =0;j<mat[0].length;j++){
+                h.put(mat[i][j] , new Pair(i, j));
                 int val = row.get(i);
                 val += mat[i][j];
                 row.put(i , val);
             }
         }
+        
         for(int j =0;j<mat[0].length;j++){
             col.put(j, 0);
             for(int i =0;i<mat.length;i++){
@@ -28,16 +26,20 @@ class Solution {
         for(int i =0;i<arr.length;i++){
             int x = arr[i];
             Pair p = h.get(x);
+
             int r = p.r;
             int c = p.c;
+
             int rsum = row.get(r);
             rsum -= x;
+            if(rsum == 0) return i;
             row.put(r, rsum);
+
             int csum = col.get(c);
             csum -= x;
+            if(csum == 0) return i;
             col.put(c, csum);
-            System.out.println(rsum + " " + csum + " " + i);
-            if(rsum == 0 || csum == 0) return i;
+
         }
         return -1;
     }
