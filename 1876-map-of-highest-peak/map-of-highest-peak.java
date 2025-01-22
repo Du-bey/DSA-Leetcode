36221dcc7cd69982116d715598d3f4efcc3dae41
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-22 15:37:44 +0530

    Time: 70 ms (12.19%) | Memory: 168.2 MB (32.19%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.java
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -1,39 +1,39 @@
 class Solution {
-    public int[][] highestPeak(int[][] mat) {
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-        int n = mat.length;
-        int m = mat[0].length;
-        Queue<Triplet> q = new LinkedList<>();
-        boolean[][] vis = new boolean[n][m];
-
+    public int[][] highestPeak(int[][] isWater) {
+        int n = isWater.length;
+        int m = isWater[0].length;
         int[][] ans = new int[n][m];
-        for(int [] r : ans){
-            Arrays.fill(r, Integer.MAX_VALUE);
+        for(int[] r : ans){
+            Arrays.fill(r, 100000000);
         }
 
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
+
+        Queue<Triplet> q = new LinkedList<>();
+
+        boolean[][] vis = new boolean[n][m];
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(mat[i][j] == 1){
+                if(isWater[i][j] == 1){
                     q.add(new Triplet(0, i, j));
                     ans[i][j] = 0;
-                    vis[i][j] = true;
                 }
             }
         }
+
         while(!q.isEmpty()){
             Triplet t = q.poll();
-            int dis = t.first;
+            int d = t.first;
             int r = t.second;
             int c = t.third;
-
             for(int i =0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr >=0 && nc >= 0 && nr < n && nc < m && mat[nr][nc] == 0 && !vis[nr][nc]){
-                    q.add(new Triplet(dis + 1, nr, nc));
+                if(nr >=0 && nc >= 0 && nr < n && nc < m && !vis[nr][nc] && isWater[nr][nc] == 0){
+                    q.add(new Triplet(d + 1, nr, nc));
+                    ans[nr][nc] = d + 1;
                     vis[nr][nc] = true;
-                    ans[nr][nc] = dis + 1;
                 }
             }
         }
