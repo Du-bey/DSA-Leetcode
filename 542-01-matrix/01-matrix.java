df1a40624d9304fa1a1f29d25134bdc04df44b25
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-22 15:40:53 +0530

    Time: 16 ms (28.97%) | Memory: 47.8 MB (23.94%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -8,15 +8,11 @@ class Solution {
         boolean[][] vis = new boolean[n][m];
 
         int[][] ans = new int[n][m];
-        for(int [] r : ans){
-            Arrays.fill(r, Integer.MAX_VALUE);
-        }
 
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(mat[i][j] == 0){
                     q.add(new Triplet(0, i, j));
-                    ans[i][j] = 0;
                     vis[i][j] = true;
                 }
             }
@@ -30,7 +26,7 @@ class Solution {
             for(int i =0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if(nr >=0 && nc >= 0 && nr < n && nc < m && mat[nr][nc] == 1 && !vis[nr][nc]){
+                if(nr >= 0 && nc >= 0 && nr < n && nc < m && mat[nr][nc] == 1 && !vis[nr][nc]){
                     q.add(new Triplet(dis + 1, nr, nc));
                     vis[nr][nc] = true;
                     ans[nr][nc] = dis + 1;
