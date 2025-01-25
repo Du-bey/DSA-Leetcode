63a7362db35ee5bdd674e2991afbec5257a547ea
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-25 20:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (28.41%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,21 +2,21 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return 0;
 
         int[][] dp = new int[m][n];
-        dp[0][0] = 1;
-        for(int i =0;i<m;i++){
-            for(int j =0;j<n;j++){
-                if(grid[i][j] == 1) dp[i][j] = 0;
-                else if(i == 0 && j == 0) dp[i][j] = 1;
-                else{
-                    if(i > 0) dp[i][j] += dp[i-1][j];
-                    if(j > 0) dp[i][j] += dp[i][j-1];
-                }
-            }
+        for(int[] r : dp){
+            Arrays.fill(r, -1);
         }
-        
-        return dp[m-1][n-1];
+        return f(m-1, n-1, m, n, dp, grid);
     }
 
+    public int f(int i, int j, int m, int n, int[][] dp, int[][] grid){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        if(grid[i][j] == 1) return 0;
+        return dp[i][j] = f(i-1, j, m, n, dp, grid) + f(i, j-1, m, n, dp, grid);
+    }
 }
