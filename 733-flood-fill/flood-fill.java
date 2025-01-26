7397e66eb43bd9482d8fbc14110773135d8011ae
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-26 16:12:05 +0530

    Time: 1 ms (45.46%) | Memory: 45 MB (48.15%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-
         int n = image.length;
         int m = image[0].length;
+
+        int[] dr = {0, 1, 0, -1};
+        int[] dc = {1, 0, -1, 0};
         int oc = image[sr][sc];
         if(oc == color) return image;
-        
         image[sr][sc] = color;
-
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr, sc));
         while(!q.isEmpty()){
             Pair p = q.poll();
             int r = p.a;
             int c = p.b;
-
             for(int i =0;i<4;i++){
                 int nr = r + dr[i];
                 int nc = c + dc[i];
@@ -24,7 +21,7 @@ class Solution {
                     q.add(new Pair(nr, nc));
                     image[nr][nc] = color;
                 }
-            }
+            }            
         }
         return image;
     }
