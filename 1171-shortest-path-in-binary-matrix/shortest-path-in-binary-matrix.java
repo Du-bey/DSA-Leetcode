68be8c7c6d655fe6f6aa070227942c9be1492ea1
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-27 01:37:27 +0530

    Time: 15 ms (68.02%) | Memory: 45.5 MB (76.92%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,35 +1,30 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n = grid.length;
-
-        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
         int[] dr = {0, -1, -1, -1, 0, 1, 1, 1};
         int[] dc = {-1, -1, 0, 1, 1, 1, 0, -1};
-
-        Queue<tuple> pq = new LinkedList<>();
-        pq.add(new tuple(1, 0, 0));
+        Queue<tuple> q = new LinkedList<>();
+        q.add(new tuple(1, 0, 0));
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
         int[][] dis = new int[n][n];
-        for(int i = 0;i<n;i++) {
-            for(int j =0;j<n;j++) {
-                dis[i][j] = (int) 1e9;
-            }
+        for(int[] r : dis){
+            Arrays.fill(r, 100000000);
         }
-        dis[0][0] = 1;
-        grid[0][0] = -1;
-        while(!pq.isEmpty()){
-            tuple t = pq.poll();
+        
+        while(!q.isEmpty()){
+            tuple t = q.poll();
             int d = t.first;
             int r = t.second;
             int c = t.third;
             if(r == n-1 && c == n-1) return d;
+
             for(int i =0;i<8;i++){
                 int newr = r + dr[i];
                 int newc = c + dc[i];
                 if(newr >=0 && newc >=0 && newr <n && newc <n && grid[newr][newc] == 0){
-                    if(dis[newr][newc] > d+1){
-                        dis[newr][newc] = d + 1;
-                        pq.add(new tuple(dis[newr][newc], newr, newc));
-                        grid[newr][newc] = -1;
+                    if(dis[newr][newc] > 1 + d){
+                        dis[newr][newc] = 1 + d;
+                        q.add(new tuple(dis[newr][newc], newr, newc));
                     }
                 }
             }
@@ -37,7 +32,6 @@ class Solution {
         return -1;
     }
 }
-
 class tuple {
     int first, second, third;
     tuple(int _first, int _second, int _third) {
