b443a48346cc617b3b02c3fb32bcf7f6de1aa834
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-01-29 01:57:02 +0530

    Time: 189 ms (6.89%) | Memory: 62.5 MB (19.55%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,26 +1,22 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        ArrayDeque<Integer> st = new ArrayDeque<>();
-        for(int i =0;i<k-1;i++){
-            while(!st.isEmpty() && nums[i] > nums[st.getLast()]){
-                st.removeLast();
-            }
-            st.addLast(i);
-        }
-        int l = 0;
+        HashMap<Integer, Integer> h = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         int[] ans = new int[n - k + 1];
-        int ind = 0;
-        for(int i = k-1;i<n;i++){
-            if(!st.isEmpty() && st.getFirst() <= i-k){
-                st.removeFirst();
-            }
-            while(!st.isEmpty() && nums[i] > nums[st.getLast()]){
-                st.removeLast();
-            }
-            st.addLast(i);
-            ans[ind++] = nums[st.getFirst()];
-            
+        for(int i =0;i<k;i++){
+            pq.add(nums[i]);
+            h.put(nums[i], h.getOrDefault(nums[i], 0) + 1);
+        }
+        ans[0] = pq.peek();
+        int st = 0;
+        for(int i = k;i<n;i++){
+            int start = nums[st++];
+            h.put(start, h.get(start) - 1);
+            pq.add(nums[i]);
+            h.put(nums[i], h.getOrDefault(nums[i], 0) + 1);
+            while(h.get(pq.peek()) <= 0) pq.poll();
+            ans[st] = pq.peek();
         }
         return ans;
     }
