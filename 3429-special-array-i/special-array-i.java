8dbd87426c8218f7edf83c6e3c6a8f45d295f104
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-01 12:11:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (56.64%) - LeetSync

--- 3429-special-array-i/special-array-i.java
+++ 3429-special-array-i/special-array-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         for(int i =1;i<nums.length;i++){
-            if(Math.abs(nums[i] % 2 - nums[i-1] % 2) != 1) return false;
+            if(nums[i] % 2 == nums[i-1] % 2) return false;
         }
         return true;
     }
