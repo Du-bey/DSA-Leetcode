1ecd7ccce544eb723f36ecbd40ce525d0db12339
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-03 09:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (98.65%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -3,15 +3,12 @@ class Solution {
         int n = nums.length;
         int ans = 1;
         int cnt = 1;
+        int cnt2 = 1;
         
         for(int i =1;i<n;i++){
             cnt = (nums[i] > nums[i-1]) ? cnt + 1 : 1;
-            ans = Math.max(ans, cnt);
-        }
-        cnt = 1;
-        for(int i =1;i<n;i++){
-            cnt = (nums[i] < nums[i-1]) ? cnt + 1 : 1;
-            ans = Math.max(ans, cnt);
+            cnt2 = (nums[i] < nums[i-1]) ? cnt2 + 1 : 1;
+            ans = Math.max(ans, Math.max(cnt, cnt2));
         }
         return ans;
     }
