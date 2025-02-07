6b9db8148d37960b200669a8b96015063b46cafb
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-08 01:27:38 +0530

    Time: 4 ms (38.06%) | Memory: 45.3 MB (9.75%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Map<Integer, Set<Character>> cols = new HashMap<>();
-        Map<Integer, Set<Character>> rows = new HashMap<>();
-        Map<String, Set<Character>> squares = new HashMap<>();
+        Map<Integer, Set<Character>> cols = new HashMap<>(9);
+        Map<Integer, Set<Character>> rows = new HashMap<>(9);
+        Map<Integer, Set<Character>> squares = new HashMap<>(9);
 
         for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
                 if (board[r][c] == '.') continue;
 
-                String squareKey = (r / 3) + "," + (c / 3);
-
-                rows.putIfAbsent(r, new HashSet<>());
-                cols.putIfAbsent(c, new HashSet<>());
-                squares.putIfAbsent(squareKey, new HashSet<>());
+                int squareKey = (r / 3) * 3 + (c / 3);
+                
+                rows.putIfAbsent(r, new HashSet<>(9));
+                cols.putIfAbsent(c, new HashSet<>(9));
+                squares.putIfAbsent(squareKey, new HashSet<>(9));
 
                 if (!rows.get(r).add(board[r][c]) ||
                     !cols.get(c).add(board[r][c]) ||
@@ -22,6 +22,7 @@ class Solution {
             }
         }
         return true;
+
 
     }
 }
