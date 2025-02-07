465d8be44a20d777271d4ab7fb08701df2f6b3e7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-07 12:27:19 +0530

    Time: 2 ms (59.47%) | Memory: 45.9 MB (83.67%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,33 +1,33 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n = nums1.length, m = nums2.length;
-        if(n > m) return findMedianSortedArrays(nums2, nums1);
-        int tSize = n + m, left = (n+m+1)/2;
-        boolean isEven = tSize % 2 == 0;
-        int l = 0, h = n;
-        while(l <= h){
-            int mid = (l+h) / 2;
-            int mid2 = left - mid;
-            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
-            if(mid > 0) l1 = nums1[mid-1];
-            if(mid2 > 0) l2 = nums2[mid2-1];
-            if(mid < n) r1 = nums1[mid];
-            if(mid2 < m) r2 = nums2[mid2];
-            if((l1 <= r2) && (l2 <= r1)){
-                if(isEven){
-                    return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                }
-                else{
-                    return Math.max(l1, l2);
-                }
-            }
-            else if(l1 < r2){
-                l = mid + 1;
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] temp = new int[n+m];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < n && j < m){
+            if(nums1[i] < nums2[j]){
+                temp[k++] = nums1[i++];
             }
             else{
-                h = mid - 1;
+                temp[k++] = nums2[j++];
             }
         }
-        return -1;
+        while(i < n){
+            temp[k++] = nums1[i++];
+        }
+        while(j < m){
+            temp[k++] = nums2[j++];
+        }
+        int size = n + m;
+        double ans = 0;
+        if(size % 2 == 1){
+            ans = temp[size/2];
+        }
+        else{
+            ans = (temp[size/2] + temp[(size - 1) /2]) / 2.0;
+        }
+        return ans;
     }
 }
