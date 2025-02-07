a9a87227c9918d3d7dc1f62e6bafa6bac2dc4efd
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-08 01:08:46 +0530

    Time: 11 ms (28.16%) | Memory: 44.9 MB (33.99%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,43 +1,25 @@
 class Solution {
-    public boolean isValidSudoku(char[][] b) {
-        int[] hash = new int[9];
-        for(int i =0;i <9;i++){
-            for(int j=0;j<9;j++){
-                char c = b[i][j];
-                if(c != '.'){
-                    if(hash[c - '1'] != 0) return false;
-                    hash[c - '1']++;
-                }
-            }
-            Arrays.fill(hash, 0);
-        }
-        for(int j=0;j<9;j++){
-            for(int i =0;i <9;i++){
-                char c = b[i][j];
-                if(c != '.'){
-                    if(hash[c - '1'] != 0) return false;
-                    hash[c - '1']++;
-                }
-            }
-            Arrays.fill(hash, 0);
-        }
+    public boolean isValidSudoku(char[][] board) {
+        Map<Integer, Set<Character>> cols = new HashMap<>();
+        Map<Integer, Set<Character>> rows = new HashMap<>();
+        Map<String, Set<Character>> squares = new HashMap<>();
 
-        for(int i =0;i <9;i+= 3){
-            for(int j=0;j<9;j+=3){
-                for(int k = i;k<i+3;k++){
-                    for(int l = j;l<j+3;l++){
-                        char c = b[k][l];
-                        if(c != '.'){
-                            if(hash[c - '1'] != 0) {
-                                return false;
-                            }
-                            hash[c - '1']++;
-                        }
-                    }
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') continue;
+
+                String squareKey = (r / 3) + "," + (c / 3);
+
+                if (rows.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c]) ||
+                    cols.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c]) ||
+                    squares.computeIfAbsent(squareKey, k -> new HashSet<>()).contains(board[r][c])) {
+                    return false;
                 }
-                Arrays.fill(hash, 0);
+
+                rows.get(r).add(board[r][c]);
+                cols.get(c).add(board[r][c]);
+                squares.get(squareKey).add(board[r][c]);
             }
-            
         }
         return true;
     }
