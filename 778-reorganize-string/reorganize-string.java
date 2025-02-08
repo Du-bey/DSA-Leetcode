fa8556d243162418c8a6b5606063abdd09745b49
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-08 13:14:56 +0530

    Time: 1 ms (92.94%) | Memory: 41.6 MB (85.42%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,55 +1,36 @@
 class Solution {
-    public String reorganizeString(String s) {
-        int n = s.length();
-        HashMap<Character, Integer> h = new HashMap<>();
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.freq - a.freq);
-        for(int i =0;i<n;i++){
-            char c = s.charAt(i);
-            int val = c - 'a';
-            h.put(c, h.getOrDefault(c, 0) + 1);
-        }
-        h.forEach((key, value) -> pq.add(new Pair(key, value)));
-        char last = '0';
-        int frequency = pq.peek().freq;
-        if(frequency > (n+1) / 2) return "";
-
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            Pair p = pq.poll();
-            char c = p.c;
-            int freq = p.freq;
-            if(c != last){
-                sb.append(c);
-                freq--;
-                if(freq > 0){
-                    pq.add(new Pair(c, freq));
-                }
-                last = c;
-            }
-            else{
-                Pair p2 = pq.poll();
-                char c2 = p2.c;
-                int freq2 = p2.freq;
-            
-                sb.append(c2);
-                freq2--;
-                if(freq2 > 0){
-                    pq.add(new Pair(c2, freq2));
-                }
-                last = c2;
-                pq.add(p);
+        public String reorganizeString(String S) {
+        int[] hash = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            hash[S.charAt(i) - 'a']++;
+        } 
+        int max = 0, letter = 0;
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] > max) {
+                max = hash[i];
+                letter = i;
             }
         }
-        return sb.toString();
-    }
-}
-
-class Pair{
-    char c;
-    int freq;
-
-    public Pair(char c, int freq){
-        this.c = c;
-        this.freq = freq;
+        if (max > (S.length() + 1) / 2) {
+            return ""; 
+        }
+        char[] res = new char[S.length()];
+        int idx = 0;
+        while (hash[letter] > 0) {
+            res[idx] = (char) (letter + 'a');
+            idx += 2;
+            hash[letter]--;
+        }
+        for (int i = 0; i < hash.length; i++) {
+            while (hash[i] > 0) {
+                if (idx >= res.length) {
+                    idx = 1;
+                }
+                res[idx] = (char) (i + 'a');
+                idx += 2;
+                hash[i]--;
+            }
+        }
+        return String.valueOf(res);
     }
 }
