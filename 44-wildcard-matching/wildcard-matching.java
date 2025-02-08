d2f3d2653fb8c3795fad39bafd78e588d1221693
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-08 16:37:33 +0530

    Time: 15 ms (72.09%) | Memory: 49.2 MB (9.10%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,31 +2,28 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int[][] dp = new int[n+1][m+1];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
-        }
+        int[][] dp = new int[n][m];
+        for(int[] r : dp) Arrays.fill(r, -1);
         return f(n-1, m-1, s, p, dp);
     }
 
     public boolean f(int i, int j, String s, String p, int[][] dp){
-        if(i < 0 && j <0) return true;
-        if(i >= 0 && j <0) return false;
+        if(i < 0 && j < 0) return true;
+        if(i >= 0 && j < 0) return false;
         if(i < 0 && j >= 0){
-            while(j >= 0){
-                if(p.charAt(j) != '*') return false;
-                j--;
+            for(int ind = 0;ind<=j;ind++){
+                if(p.charAt(ind) != '*') return false;
             }
             return true;
         }
-        if(dp[i][j] != -1) return dp[i][j] == 0 ? false : true;
 
+        if(dp[i][j] != -1) return dp[i][j] == 1 ? true : false;
         boolean ans = false;
         if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
             ans = f(i-1, j-1, s, p, dp);
         }
         else if(p.charAt(j) == '*'){
-            ans = (f(i-1, j, s, p, dp) || f(i, j-1, s, p, dp));
+            ans = f(i, j-1, s, p, dp) || f(i-1, j, s, p, dp);
         }
         dp[i][j] = ans ? 1 : 0;
         return ans;
