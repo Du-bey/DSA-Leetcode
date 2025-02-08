9406d1757670ad0ebb9c58b3efc32b926436fed7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-08 12:53:27 +0530

    Time: 78 ms (65.07%) | Memory: 97.8 MB (86.99%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.java
+++ 2434-design-a-number-container-system/design-a-number-container-system.java
@@ -1,35 +1,29 @@
+
 class NumberContainers {
-    HashMap<Integer, PriorityQueue<Integer>> h;
+    HashMap<Integer, TreeSet<Integer>> h;
     HashMap<Integer, Integer> ind;
+
     public NumberContainers() {
         h = new HashMap<>();
         ind = new HashMap<>();
     }
     
     public void change(int index, int num) {
-        h.putIfAbsent(num, new PriorityQueue<>());
-        PriorityQueue<Integer> pq = h.get(num);
-        pq.add(index);
-        h.put(num, pq);
+        if (ind.containsKey(index)) {
+            int oldNum = ind.get(index);
+            if (oldNum != num) {
+                h.get(oldNum).remove(index);
+                if (h.get(oldNum).isEmpty()) {
+                    h.remove(oldNum);
+                }
+            }
+        }
+        h.putIfAbsent(num, new TreeSet<>());
+        h.get(num).add(index);
         ind.put(index, num);
     }
     
     public int find(int num) {
-        if(h.containsKey(num)){
-            PriorityQueue<Integer> pq = h.get(num);
-            while(!pq.isEmpty()){
-                int index = pq.peek();
-                if(ind.get(index) == num) return index;
-                pq.poll();
-            }
-        }
-        return -1;
+        return h.containsKey(num) && !h.get(num).isEmpty() ? h.get(num).first() : -1;
     }
 }
-
-/**
- * Your NumberContainers object will be instantiated and called as such:
- * NumberContainers obj = new NumberContainers();
- * obj.change(index,number);
- * int param_2 = obj.find(number);
- */
