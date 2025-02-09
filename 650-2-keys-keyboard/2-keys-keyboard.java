3f8a92eb30afa51bc6274a8a8be22d72a3d7899c
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-10 01:53:51 +0530

    Time: 19 ms (24.77%) | Memory: 55.5 MB (9.75%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,15 +1,21 @@
 class Solution {
     public int minSteps(int n) {
         if(n == 1) return 0;
-        return 1 + f(1, 1, n);
+        int[][] dp = new int[n+1][n+1];
+        for(int [] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return 1 + f(1, 1, n, dp);
     }
 
-    public int f(int c, int p, int n){
+    public int f(int c, int p, int n, int[][] dp){
         if(c == n) return 0;
         if(c > n) return 100000;
 
-        int v1 = 1 + f(c+p, p, n);
-        int v2 = 2 + f(c+c, c, n);
-        return Math.min(v1, v2);
+        if(dp[c][p] != -1) return dp[c][p];
+
+        int v1 = 1 + f(c+p, p, n, dp);
+        int v2 = 2 + f(c+c, c, n, dp);
+        return dp[c][p] = Math.min(v1, v2);
     }
 }
