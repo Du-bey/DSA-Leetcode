eb204c17f60649810e3a1256ec727ceb73795fa2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-09 13:41:33 +0530

    Time: 2349 ms (38.58%) | Memory: 41.2 MB (94.39%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -8,17 +8,15 @@ class Solution {
 
     public int f(int i, String s, int n, int[] dp){
         if(i == n) return 0;
-
         if(dp[i] != -1) return dp[i];
         int min = Integer.MAX_VALUE;
-        for(int j =i;j<n;j++){
+        for(int j=i;j<n;j++){
             if(isPalindrome(i, s, j)){
                 int cost = 1 + f(j+1, s, n, dp);
                 min = Math.min(min, cost);
             }
         }
-        dp[i] = min;
-        return dp[i];
+        return dp[i] = min;
     }
 
     public boolean isPalindrome(int i, String s, int j){
