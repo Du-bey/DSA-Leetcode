3b27436231240ce4d221cc9182e573a91798fda3
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-09 17:59:04 +0530

    Time: 2 ms (65.00%) | Memory: 43.1 MB (16.67%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,33 +1,21 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String input) {
-        List<Integer> ret = new LinkedList<Integer>();
-        for (int i=0; i<input.length(); i++) {
-            if (input.charAt(i) == '-' ||
-                input.charAt(i) == '*' ||
-                input.charAt(i) == '+' ) {
-                String part1 = input.substring(0, i);
-                String part2 = input.substring(i+1);
-                List<Integer> part1Ret = diffWaysToCompute(part1);
-                List<Integer> part2Ret = diffWaysToCompute(part2);
-                for (Integer p1 :   part1Ret) {
-                    for (Integer p2 :   part2Ret) {
-                        int c = 0;
-                        switch (input.charAt(i)) {
-                            case '+': c = p1+p2;
-                                break;
-                            case '-': c = p1-p2;
-                                break;
-                            case '*': c = p1*p2;
-                                break;
-                        }
-                        ret.add(c);
+    public List<Integer> diffWaysToCompute(String expression) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < expression.length(); ++i) {
+            char oper = expression.charAt(i);
+            if (oper == '+' || oper == '-' || oper == '*') {
+                List<Integer> s1 = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> s2 = diffWaysToCompute(expression.substring(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') res.add(a + b);
+                        else if (oper == '-') res.add(a - b);
+                        else if (oper == '*') res.add(a * b);
                     }
                 }
             }
         }
-        if (ret.size() == 0) {
-            ret.add(Integer.valueOf(input));
-        }
-        return ret;
+        if (res.isEmpty()) res.add(Integer.parseInt(expression));
+        return res;
     }
 }
