8c4144ab4f005c4419ad65becde324ad66c8b09d
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-09 10:33:36 +0530

    Time: 1 ms (99.73%) | Memory: 44 MB (83.70%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -10,13 +10,8 @@ class Solution {
     }
 
     public int f(int i, int j, int n, List<List<Integer>> tri, int[][] dp){
-        int val = tri.get(i).get(j);
-        if(i == n-1) return val;
-
+        if(i > n-1) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int d = f(i+1, j, n, tri, dp);
-        int dr = f(i+1, j+1, n, tri, dp);
-        int ans = Math.min(d, dr);
-        return dp[i][j] = val + ans;
+        return dp[i][j] = tri.get(i).get(j) + Math.min(f(i+1, j, n, tri, dp), f(i+1, j+1, n, tri, dp));
     }
 }
