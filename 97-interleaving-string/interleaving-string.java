9abf8e0f7dd69812000d10ffadb32e4ce471b34c
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-11 23:11:02 +0530

    Time: 8 ms (22.21%) | Memory: 41.7 MB (68.61%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -7,24 +7,21 @@ class Solution {
         m = s2.length();
         l = s3.length();
         if (n + m != l) return false;
-        int[][] dp = new int[n+1][m+1];
-        for(int[] r : dp) Arrays.fill(r, -1);
-        return f(0, 0, s1, s2, s3, dp);
-    }
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[n][m] = true;
+        for(int i = n;i>=0;i--){
+            for(int j =m;j>=0;j--){
+                int k = i + j;
+                boolean ans = false;
+                if(i < n && s1.charAt(i) == s3.charAt(k) && dp[i+1][j]){
+                    dp[i][j] = true;
+                }
+                if(j < m && s2.charAt(j) == s3.charAt(k) && dp[i][j+1]){
+                    dp[i][j] = true;
+                }
+            }
+        }
 
-    public boolean f(int i, int j, String s1, String s2, String s3, int[][] dp){
-        if(i == n && j == m) return true;
-        if(dp[i][j] != -1) return dp[i][j] == 1 ? true : false;
-        int k = i + j;
-        char c = s3.charAt(k);
-        boolean ans = false;
-        if(i < n && s1.charAt(i) == c && f(i+1, j, s1, s2, s3, dp)){
-            ans = true;
-        }
-        if(j < m && s2.charAt(j) == c && f(i, j+1, s1, s2, s3, dp)){
-            ans = true;
-        }
-        dp[i][j] = ans ? 1 : 0;
-        return ans;
+        return dp[0][0];
     }
 }
