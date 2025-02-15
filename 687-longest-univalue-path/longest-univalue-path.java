7bc04a5c363f3096db556c86f43695132d2443c6
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-15 21:47:52 +0530

    Time: 2 ms (94.44%) | Memory: 47.1 MB (35.71%) - LeetSync

--- 687-longest-univalue-path/longest-univalue-path.java
+++ 687-longest-univalue-path/longest-univalue-path.java
@@ -1,17 +1,28 @@
 class Solution {
-    int ans = 0;
+    int max = 0;
     public int longestUnivaluePath(TreeNode root) {
-        if(root == null) return 0;
-        f(root, Integer.MAX_VALUE);
-        return ans;
+        int val = dfs(root);
+        return max;
     }
 
-    public int f(TreeNode root, int val){
+    public int dfs(TreeNode root) {
         if(root == null) return 0;
-        int left = f(root.left, root.val);
-        int right = f(root.right, root.val);
-        ans = Math.max(ans, left + right);
-        if(root.val != val) return 0;
-        return 1 + Math.max(left, right);
+
+        int leftLen = dfs(root.left);
+        int rightLen = dfs(root.right);
+
+        int leftPath = 0, rightPath = 0;
+
+        if(root.left != null && root.left.val == root.val) {
+            leftPath = leftLen + 1;
+        }
+
+        if(root.right != null && root.right.val == root.val) {
+            rightPath = rightLen + 1;
+        }
+
+        max = Math.max(leftPath + rightPath, max);
+
+        return Math.max(leftPath, rightPath);
     }
 }
