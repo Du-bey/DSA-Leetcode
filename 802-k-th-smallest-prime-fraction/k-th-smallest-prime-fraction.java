9b9e8963f67f25829df27574c7e561283c3832a2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-15 15:08:59 +0530

    Time: 261 ms (50.00%) | Memory: 49 MB (44.76%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -1,18 +1,30 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
         int n = arr.length;
-        PriorityQueue<Triplet> pq = new PriorityQueue<>((a, b) -> Double.compare(b.d, a.d));
-        for(int i =0;i<n-1;i++){
-            for(int j =i+1;j<n;j++){
-                double frac = (double) arr[i] / arr[j];
-                pq.add(new Triplet(frac, arr[i], arr[j]));
-                if(pq.size() > k) pq.poll();
+        PriorityQueue<Triplet> pq = new PriorityQueue<>((a, b) -> Double.compare(a.d, b.d));
+        pq.add(new Triplet((double) arr[0] / arr[n-1], 0, n-1));
+        boolean[][] vis = new boolean[n][n];
+        vis[0][n-1] = true;
+        while(--k > 0){
+            Triplet p = pq.poll();
+            int i = p.x;
+            int j = p.y;
+            if(i+1 < j && i+1 < n && !vis[i+1][j]){
+                double frac = (double) arr[i+1] / arr[j];
+                pq.add(new Triplet(frac, i+1, j));
+                vis[i+1][j] = true;
+            }
+
+            if(j - 1 > i && !vis[i][j-1]){
+                double frac = (double) arr[i] / arr[j-1];
+                pq.add(new Triplet(frac, i, j-1));
+                vis[i][j-1] = true;
             }
         }
         Triplet t = pq.poll();
         int[] ans = new int[2];
-        ans[0] = t.x;
-        ans[1] = t.y;
+        ans[0] = arr[t.x];
+        ans[1] = arr[t.y];
         return ans;
     }
 }
