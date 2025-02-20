cc704d6fc640663b7e5cc25984d8c0cad5a36669
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-20 12:36:13 +0530

    Time: 275 ms (9.27%) | Memory: 45.7 MB (7.96%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    String ans;
+    public String findDifferentBinaryString(String[] nums) {
+        Set<String> set = new HashSet<>(Arrays.asList(nums));
+        solve(set, "", nums[0].length());
+        return ans;
+    }
+
+    public void solve(Set<String> set, String s, int n){
+        if(s.length() == n){
+            if(!set.contains(s)){
+                ans = s;
+                return;
+            }
+            return;
+        }
+
+        solve(set, s + '0', n);
+        solve(set, s + '1', n);
+    }
+}
