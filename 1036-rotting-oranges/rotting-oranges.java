9c3fbb973c7c5ab801e087db317fb3a07a1298cc
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-26 01:22:03 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (29.55%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,55 +2,52 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-
-        boolean[][] vis = new boolean[n][m];
-        Queue<Triplet> q = new LinkedList<>();
-        for(int i =0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 2){
-                    vis[i][j] = true;
-                    q.add(new Triplet(0, i, j));
-                }
-            }
-        }
+        Queue<Pair> q = new LinkedList<>();
         int ans = 0;
-        while(!q.isEmpty()){
-            Triplet t = q.poll();
-            int dis = t.a;
-            int r = t.b;
-            int c = t.c;
-            ans = Math.max(ans, dis);
-            for(int i =0;i<4;i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr >=0 && nc >= 0 && nr < n && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
-                    q.add(new Triplet(dis + 1, nr, nc));
-                    vis[nr][nc] = true;
+        int[] delr = {0, 1, 0, -1};
+        int[] delc = {1, 0, -1, 0};
+        for(int i =0;i<n;i++){
+            for(int j =0;j<m;j++){
+                if(grid[i][j] == 2){
+                    q.add(new Pair(i, j, 0));
                 }
             }
         }
+
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int r = p.r;
+            int c = p.c;
+            int l = p.l;
+            ans = Math.max(ans, l);
+
+            for(int ind= 0;ind<4;ind++){
+                int nr = r + delr[ind];
+                int nc = c + delc[ind];
+                if(nr >=0 && nc >=0 && nr <n && nc < m && grid[nr][nc] == 1){
+                    grid[nr][nc] = 2;
+                    q.add(new Pair(nr, nc, l+1));
+                }
+            }
+        }
+
         for(int i =0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
-                    return -1;
-                }
+                if(grid[i][j] == 1) return -1;
             }
         }
         return ans;
     }
 }
 
-public class Triplet {
-    int a;
-    int b;
-    int c;
 
-    public Triplet(int a, int b, int c) {
-        this.a = a;
-        this.b = b;
+class Pair{
+    int r;
+    int c;
+    int l;
+    public Pair(int r, int c, int l){
+        this.r = r;
         this.c = c;
+        this.l = l;
     }
 }
