e3d6d73144521406575c3907e0ce3088b62704b2
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-26 00:30:42 +0530

    Time: 4 ms (15.01%) | Memory: 44.9 MB (31.52%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,47 +1,46 @@
 class Solution {
+    int ans = 0;
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         boolean [][] vis = new boolean[n][m];
-        int cnt = 0;
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(!vis[i][j] && grid[i][j] == 1){
-                    int size = bfs(i, j, vis, grid, n, m);
                     vis[i][j] = true;
-                    cnt = Math.max(cnt, size);
+                    bfs(i, j, vis, grid, n, m);
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 
-    public int bfs(int i, int j, boolean [][] vis, int[][] grid, int n, int m){
+    public void bfs(int i, int j, boolean [][] vis, int[][] grid, int n, int m){
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(i, j));
-        int cnt = 1;
         int[] delr = {0, 1, 0, -1};
         int[] delc = {1, 0, -1, 0};
-        vis[i][j] = true;
+        int cnt = 0;
         while(!q.isEmpty()){
             Pair p = q.poll();
             int r = p.r;
             int c = p.c;
-            
+            cnt++;
             for(int ind= 0;ind<4;ind++){
                 int nr = r + delr[ind];
                 int nc = c + delc[ind];
                 if(nr >=0 && nc >=0 && nr <n && nc < m && !vis[nr][nc] && grid[nr][nc] == 1){
-                    cnt++;
-                    q.add(new Pair(nr, nc));
                     vis[nr][nc] = true;
+                    q.add(new Pair(nr, nc));
                 }
             }
         }
-        return cnt;
+        ans = Math.max(ans, cnt);
+        
     }
 
 }
+
 class Pair{
     int r;
     int c;
