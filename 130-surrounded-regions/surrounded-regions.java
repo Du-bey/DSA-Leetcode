85a3c37ced57d353b5dd8fc695559e499cb2717a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-26 01:27:06 +0530

    Time: 4 ms (22.20%) | Memory: 45.8 MB (28.92%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,17 +1,16 @@
 class Solution {
-    public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
-
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-
+    public void solve(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
         Queue<Pair> q = new LinkedList<>();
+        int ans = 0;
+        int[] delr = {0, 1, 0, -1};
+        int[] delc = {1, 0, -1, 0};
         boolean[][] vis = new boolean[n][m];
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
-                if(i == 0 || i == n-1 || j == 0 || j == m-1){
-                    if(board[i][j] == 'O'){
+                if(i == 0 || j == 0 || i == n-1 || j == m-1){
+                    if(grid[i][j] == 'O'){
                         q.add(new Pair(i, j));
                         vis[i][j] = true;
                     }
@@ -21,33 +20,33 @@ class Solution {
 
         while(!q.isEmpty()){
             Pair p = q.poll();
-            int r = p.a;
-            int c = p.b;
-            for(int i =0;i<4;i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr >=0 && nr < n && nc >= 0 && nc < m && board[nr][nc] == 'O'  && !vis[nr][nc]){
-                    q.add(new Pair(nr, nc));
+            int r = p.r;
+            int c = p.c;
+
+            for(int ind= 0;ind<4;ind++){
+                int nr = r + delr[ind];
+                int nc = c + delc[ind];
+                if(nr >=0 && nc >=0 && nr <n && nc < m && !vis[nr][nc] && grid[nr][nc] == 'O'){
                     vis[nr][nc] = true;
+                    q.add(new Pair(nr, nc));
                 }
             }
         }
+
         for(int i =0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(board[i][j] == 'O' && !vis[i][j]){
-                    board[i][j] = 'X';
-                }
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 'O' && !vis[i][j]) grid[i][j] = 'X';
             }
         }
     }
 }
 
-public class Pair {
-    int a;
-    int b;
 
-    public Pair(int a, int b) {
-        this.a = a;
-        this.b = b;
+class Pair{
+    int r;
+    int c;
+    public Pair(int r, int c){
+        this.r = r;
+        this.c = c;
     }
 }
