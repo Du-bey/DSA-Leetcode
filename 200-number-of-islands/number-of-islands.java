11b683d24d78c6caecba1fa562a9460801fc72ce
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-02-26 00:22:01 +0530

    Time: 6 ms (25.40%) | Memory: 53.3 MB (5.71%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,50 +2,48 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
-        boolean[][] vis = new boolean[n][m];
-        
-        int ans = 0;
+        boolean [][] vis = new boolean[n][m];
+        int cnt = 0;
         for(int i =0;i<n;i++){
             for(int j =0;j<m;j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
-                    ans++;
                     vis[i][j] = true;
-                    bfs(vis, i, j, grid, n, m);
+                    bfs(i, j, vis, grid, n, m);
+                    cnt++;
                 }
             }
         }
-        return ans;
+        return cnt;
     }
-    
-    public void bfs(boolean[][] vis, int i, int j, char[][] grid, int n, int m){
+
+    public void bfs(int i, int j, boolean [][] vis, char[][] grid, int n, int m){
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(i, j));
-    
         int[] delr = {0, 1, 0, -1};
         int[] delc = {1, 0, -1, 0};
         while(!q.isEmpty()){
             Pair p = q.poll();
             int r = p.r;
             int c = p.c;
-            for(int ind =0;ind<4;ind++){
+            
+            for(int ind= 0;ind<4;ind++){
                 int nr = r + delr[ind];
                 int nc = c + delc[ind];
-                if(nr < n && nc < m && nr >=0 && nc >=0 && grid[nr][nc] == '1' && !vis[nr][nc]){
-                    
+                if(nr >=0 && nc >=0 && nr <n && nc < m && !vis[nr][nc] && grid[nr][nc] == '1'){
                     vis[nr][nc] = true;
                     q.add(new Pair(nr, nc));
                 }
             }
         }
+        
     }
-}
 
+}
 class Pair{
     int r;
     int c;
     public Pair(int r, int c){
         this.r = r;
         this.c = c;
-    }   
+    }
 }
