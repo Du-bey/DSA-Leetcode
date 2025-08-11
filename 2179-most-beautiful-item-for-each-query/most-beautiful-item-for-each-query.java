0418491057abeea71ad0798bb7894b61cc73c2c4
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-11 14:59:41 +0530

    Time: 44 ms (79.22%) | Memory: 81.9 MB (6.49%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.java
@@ -1,33 +1,38 @@
-import java.util.*;
-
-public class Solution {
+class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        int maxI = Integer.MAX_VALUE;
-        List<int[]> res = new ArrayList<>();
-        res.add(new int[] {0, 0, maxI});
+        int n = items.length;
+        int qn = queries.length;
+        int[] ans = new int[qn];
 
-        Arrays.sort(items, Comparator.comparingInt(a -> a[0]));
+        Arrays.sort(items, (a, b) -> a[0] - b[0]);
 
-        for (int[] item : items) {
-            int price = item[0];
-            int beauty = item[1];
-            int[] lastBracket = res.get(res.size() - 1);
-
-            if (beauty > lastBracket[1]) {
-                lastBracket[2] = price;
-                res.add(new int[] {price, beauty, maxI});
-            }
+        int max = 0;
+        for(int i =0;i<n;i++){
+            max = Math.max(max, items[i][1]);
+            items[i][1] = max;
         }
 
-        int[] ans = new int[queries.length];
+        for(int i =0;i<qn;i++){
+            ans[i] = bs(items, queries[i]);
+        }
+        return ans;
+    }
 
-        for (int j = 0; j < queries.length; j++) {
-            int x = queries[j];
-            for (int i = res.size() - 1; i >= 0; i--) {
-                if (res.get(i)[0] <= x) {
-                    ans[j] = res.get(i)[1];
-                    break;
-                }
+    public int bs(int[][] items, int q){
+        int n = items.length;
+        int l = 0;
+        int h = n-1;
+
+        int ans = 0;
+        while(l <= h){
+            int mid = l + (h-l)/2;
+            int mp = items[mid][0];
+            if(mp > q){
+                h = mid - 1;
+            }
+            else{
+                ans = items[mid][1];
+                l = mid + 1;
             }
         }
         return ans;
