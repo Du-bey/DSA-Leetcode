7192ddaa7b59e31195ea7f179d0142899c5df92b
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-11 16:51:58 +0530

    Time: 26 ms (79.44%) | Memory: 57.4 MB (25.73%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,15 +1,28 @@
 class Solution {
-    public int minimizedMaximum(int n, int[] A) {
-        int left = 1, right = 100000;
-        while (left < right) {
-            int mid = (left + right) / 2, sum = 0;
-            for (int a : A)
-                sum += (a + mid - 1) / mid;
-            if (sum > n)
-                left = mid + 1;
-            else
-                right = mid;
+    public int minimizedMaximum(int n, int[] q) {
+        int l = 1;
+        int h = 1000000;
+
+        int ans = 0;
+        while(l <= h){
+            int m = (l+h) /2;
+            if(isPossible(m, n, q)){
+                h = m - 1;
+                ans = m;
+            }
+            else{
+                l = m + 1;
+            }
         }
-        return left;
+        return ans;
+    }
+
+    public boolean isPossible(int m, int n, int[] q){
+        int cnt = 0;
+        for(int i =0;i<q.length;i++){
+            cnt += ((q[i] + m - 1) / m);
+            if(cnt > n) return false;
+        }
+        return true;
     }
 }
