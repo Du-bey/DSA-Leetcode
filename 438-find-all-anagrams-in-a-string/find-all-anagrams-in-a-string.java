15fbee2861b720129f100e5341f359a4626808d7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-12 23:36:31 +0530

    Time: 8 ms (95.84%) | Memory: 44.8 MB (82.29%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,38 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        int[] pfreq = new int[26];
+        int pn = p.length();
         int n = s.length();
-        int m = p.length();
-        List<Integer> ans = new ArrayList<>();
-        if(n < m) return ans;
-        HashMap<Character, Integer> pmap = new HashMap<>();
-        HashMap<Character, Integer> smap = new HashMap<>();
-        for(int i =0;i<m;i++){
-            char ch = p.charAt(i);
-            pmap.put(ch, pmap.getOrDefault(ch,0) + 1);
-        }
-
-        for(int i =0;i<m;i++){
-            char ch = s.charAt(i);
-            smap.put(ch, smap.getOrDefault(ch,0) + 1);
-        }
-
-        int i = 0;
-    
         
-        for(int j = m;j <n;j++){
-            if(smap.equals(pmap)) ans.add(i);
-            char cj = s.charAt(j);
-            char ci = s.charAt(i);
-            smap.put(cj, smap.getOrDefault(cj,0) + 1);
-            if(smap.get(ci) == 1){
-                smap.remove(ci);
-            }
-            else{
-                smap.put(ci, smap.get(ci) - 1);
-            }
-            i++;
+        for(char c : p.toCharArray()){
+            pfreq[c - 'a']++;
         }
-        if(smap.equals(pmap)) ans.add(i);
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        for(int j =0;j<n;j++){
+            char c = s.charAt(j);
+            pfreq[c - 'a']--;
+            if(j - i + 1 == pn){
+                if(allZeroes(pfreq)){
+                    ans.add(i);
+                }
+
+                pfreq[s.charAt(i) - 'a']++;
+                i++;
+            }
+        }
         return ans;
+    }
+
+    public boolean allZeroes(int[] pfreq){
+        for(int i = 0;i<26;i++){
+            if(pfreq[i] != 0) return false;
+        }
+        return true;
     }
 }
