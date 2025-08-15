7a69fc60b2d6cb335dc5f1835b2aaa5690aab947
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-15 16:13:02 +0530

    Time: 7 ms (79.46%) | Memory: 59.4 MB (41.09%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.java
@@ -1,25 +1,22 @@
 class Solution {
     public int minSwaps(int[] nums) {
         int n = nums.length;
-        int N = n*2;
         int sum = 0;
-        int[] num = new int[N];
 
         for(int i = 0;i<n;i++){
             sum += nums[i];
-            num[i] = nums[i];
-            num[i+n] = nums[i];
         }
         
         int l = 0;
         int ans = n;
         int temp = 0;
 
-        for(int r =0;r < N;r++){
-            temp += num[r];
+        for(int r =0;r < 2 * n;r++){
+            temp += nums[r % n];
 
             while(r - l + 1 > sum){
-                temp -= num[l++];
+                temp -= nums[l % n];
+                l++;
             }
 
             int win = r - l + 1;
