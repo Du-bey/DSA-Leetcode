3d89b43689b019a05f94563cb98b7f64d5f1b5db
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-15 21:19:43 +0530

    Time: 3 ms (24.61%) | Memory: 45.5 MB (60.83%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        if (k == 1) return nums;
+        if(k == 1) return nums;
         int n = nums.length;
+
         int[] res = new int[n-k+1];
-        Arrays.fill(res,-1);
-        int count = 1;
-        for(int i=0; i<n-1; i++){
-            if(nums[i+1] == nums[i]+1){
-                count++;
-            }else{
-                count = 1;
+        Deque<Integer> q = new LinkedList<>();
+        
+        int ind = 0;
+        for(int i = 0;i< n;i++){
+            if(q.size() == k){
+                q.pollFirst();
             }
 
-            if (count >= k){
-                res[i-k+2] = nums[i+1];
+            if(!q.isEmpty() && q.peekLast() + 1 != nums[i]){
+                q.clear();
+            }
+
+            q.offerLast(nums[i]);
+            if(i >= k -1){
+                res[ind++] = (q.size() == k) ? q.peekLast() : -1;
             }
         }
-
         return res;
     }
 }
