19f03028095433808ec653d260f8cbe390b82b9c
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-16 02:38:00 +0530

    Time: 30 ms (37.53%) | Memory: 57.1 MB (89.70%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.java
+++ 2868-continuous-subarrays/continuous-subarrays.java
@@ -1,37 +1,20 @@
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        int n = nums.length;
-        PriorityQueue<Pair> min = new PriorityQueue<>((a, b) -> a.a - b.a);
-        PriorityQueue<Pair> max = new PriorityQueue<>((a, b) -> b.a - a.a);
-        int i =0;
-        int j =0;
         long ans = 0;
-        while(j < n){
-            min.add(new Pair(nums[j], j));
-            max.add(new Pair(nums[j], j));
-            while(Math.abs(max.peek().a - min.peek().a) > 2){
-                
-                i++; 
-                while(!max.isEmpty() && max.peek().b < i){
-                    max.poll();
-                }
+        TreeMap<Integer, Integer> h = new TreeMap<>();
+        int n = nums.length;
 
-                while(!min.isEmpty() && min.peek().b < i){
-                    min.poll();
-                }
+        int i = 0;
+        for(int j =0;j<n;j++){
+            h.put(nums[j], h.getOrDefault(nums[j], 0) + 1);
+            while(Math.abs(h.lastKey() - h.firstKey()) > 2){
+                h.put(nums[i], h.get(nums[i]) - 1);
+                if(h.get(nums[i]) == 0) h.remove(nums[i]);
+                i++;
             }
+
             ans += j - i + 1;
-            j++;
         }
         return ans;
-    }
-}
-
-class Pair{
-    int a;
-    int b;
-    public Pair(int a, int b){
-        this.a = a;
-        this.b = b;
     }
 }
