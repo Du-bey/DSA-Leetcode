c9deb7cdd04ac86cd6c8b43d44981f7b4d4864a1
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-28 19:12:51 +0530

    Time: 9 ms (13.30%) | Memory: 46.1 MB (11.01%) - LeetSync

--- 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.java
@@ -1,40 +1,32 @@
 class Solution {
-    int n;
-    int m;
     public int[][] sortMatrix(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
+        int n = grid.length;
+        Map<Integer, List<Integer>> mp = new HashMap<>();
 
-        for(int r = 0;r < n;r++){
-            sort(r, 0, grid, false);
-        }
-
-        for(int c = 1; c<m;c++){
-            sort(0, c, grid, true);
-        }
-        return grid;
-    }
-
-    public void sort(int r, int c, int[][] grid, boolean isAsc){
-        int i = r;
-        int j = c;
-        List<Integer> temp = new ArrayList<>();
-        while(i < n && j < m){
-            temp.add(grid[i++][j++]);
-        }
-        if(isAsc){
-            Collections.sort(temp);
-        }
-        else{
-            temp.sort(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int diag = i - j;
+                mp.computeIfAbsent(diag, k -> new ArrayList<>()).add(grid[i][j]);
+            }
         }
 
-        i = r;
-        j = c;
-        for(int x : temp){
-            grid[i][j] = x;
-            i++;
-            j++;
+        for (Map.Entry<Integer, List<Integer>> entry : mp.entrySet()) {
+            List<Integer> list = entry.getValue();
+            if (entry.getKey() >= 0) {
+                Collections.sort(list);
+            } else {
+                list.sort(Collections.reverseOrder());
+            }
         }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int diag = i - j;
+                List<Integer> list = mp.get(diag);
+                grid[i][j] = list.remove(list.size() - 1);
+            }
+        }
+
+        return grid;    
     }
 }
