5dfdae1c224e61ae27eecc2df62029d172f964f4
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-30 02:35:51 +0530

    Time: 51 ms (53.48%) | Memory: 55.9 MB (98.76%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,35 +1,21 @@
 class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
-        
         HashMap<Integer, Integer> h = new HashMap<>();
         for(int x : arr){
             h.put(x, h.getOrDefault(x, 0) + 1);
         }
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.freq - b.freq);
-        for(int i : h.keySet()){
-            pq.add(new Pair(h.get(i) , i));
-        }
-        while(!pq.isEmpty() && k > 0){
-            Pair p = pq.poll();
-            int freq = p.freq;
-            if(freq > k){
-                freq -= k;
-                pq.add(new Pair(freq, p.b));
-                break;
-            }
-            else{
-                k -= freq;
-            }
-        }
-        return pq.size();
-    }
-}
 
-class Pair{
-    int freq;
-    int b;
-    public Pair(int freq, int b){
-        this.freq = freq;
-        this.b = b;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int x : h.keySet()){
+            pq.add(h.get(x));
+        }
+
+        while(k > 0){
+            int freq = pq.poll();
+            k -= freq;
+        }
+
+        int ans = pq.size();
+        return k == 0 ? ans : ans + 1;
     }
 }
