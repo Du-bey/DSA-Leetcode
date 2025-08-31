5ab9fc1d21f06f975af86e36fb20e66c341e5da7
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-08-31 15:38:43 +0530

    Time: 5 ms (13.29%) | Memory: 42.5 MB (22.38%) - LeetSync

---
+++ 3494-minimum-cost-for-cutting-cake-i/minimum-cost-for-cutting-cake-i.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minimumCost(int m, int n, int[] h, int[] v) {
+        Arrays.sort(h);
+        Arrays.sort(v);
+
+        int i = m-2;
+        int j = n-2;
+        int vcut = 0;
+        int hcut = 0;
+        int ans = 0;
+        while(i >= 0 && j >= 0){
+            if(h[i] < v[j]){
+                vcut++;
+                ans += v[j--] * (hcut + 1);
+            }
+            else{
+                hcut++;
+                ans += h[i--] * (vcut + 1);
+            }
+        }
+
+        while(i >= 0){
+            hcut++;
+            ans += h[i--] * (vcut + 1);
+            System.out.println(ans);
+        }
+
+        while(j >= 0){
+            vcut++;
+            ans += v[j--] * (hcut + 1);
+        }
+
+        return ans;
+        
+    }
+}
