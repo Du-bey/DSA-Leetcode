099b11e4c0b34201abadb8275d179cbe7af6f13a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-09-05 14:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (50.27%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        if(grid[0][0] == 1 || grid[m-1][n-1] == 1) return 0;
 
-        int[][] dp = new int[m][n];
-        for(int[] r : dp){
-            Arrays.fill(r, -1);
-        }
-        return f(m-1, n-1, m, n, dp, grid);
+    int[][] dp;
+    public int uniquePathsWithObstacles(int[][] g) {
+        int m = g.length;
+        int n = g[0].length;
+        dp = new int[m][n];
+        for(int[] r : dp) Arrays.fill(r, -1);
+        return f(m-1, n-1, g);
     }
 
-    public int f(int i, int j, int m, int n, int[][] dp, int[][] grid){
+    public int f(int i, int j, int[][] g){
+        if(i < 0 || j < 0 || g[i][j] == 1) return 0;
         if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
-        if(grid[i][j] == 1) return 0;
-        return dp[i][j] = f(i-1, j, m, n, dp, grid) + f(i, j-1, m, n, dp, grid);
+        int up = f(i-1, j, g);
+        int left = f(i, j-1, g);
+        dp[i][j] = up + left;
+        return dp[i][j];
     }
 }
