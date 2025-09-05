e92240ba6594d47821226200c7a7ce048316a6cd
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-09-06 01:35:55 +0530

    Time: 2 ms (52.41%) | Memory: 43.1 MB (18.72%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.java
@@ -6,52 +6,52 @@ class Solution {
     public int maxProductPath(int[][] grid) {
         n = grid.length;
         m = grid[0].length;
-        dp = new long[n][m][2];
+        dp = new long[n+1][m+1][2];
 
-        for(long[][] r : dp){
-            for(long[] c : r){
-                c[0] = Long.MAX_VALUE;
-                c[1] = Long.MIN_VALUE;
+        int last = grid[n-1][m-1];
+
+        dp[n-1][m-1] = new long[]{last, last};
+        for(int i =n-2; i>=0;i--){
+            long max = Math.max(grid[i][m-1] * dp[i+1][m-1][0], grid[i][m-1] * dp[i+1][m-1][1]);
+            long min = Math.min(grid[i][m-1] * dp[i+1][m-1][0], grid[i][m-1] * dp[i+1][m-1][1]);
+            dp[i][m-1] = new long[]{min, max};
+        }
+
+        for(int i = m-2;i>=0;i--){
+            long max = Math.max(grid[n-1][i] * dp[n-1][i+1][0], grid[n-1][i] * dp[n-1][i+1][1]);
+            long min = Math.min(grid[n-1][i] * dp[n-1][i+1][0], grid[n-1][i] * dp[n-1][i+1][1]);
+            dp[n-1][i] = new long[]{min, max};
+        }
+
+        
+        for(int i = n-2;i>=0;i--){
+            for(int j = m-2;j>=0;j--){
+                int val = grid[i][j];
+                long max = Long.MIN_VALUE;
+                long min = Long.MAX_VALUE;
+
+                long[] down = dp[i+1][j];
+                long dmax = down[1];
+                long dmin = down[0];
+
+                max = Math.max(max, Math.max(val * dmax, val * dmin));
+                min = Math.min(min, Math.min(val * dmax, val * dmin));
+
+                long[] r = dp[i][j+1];
+                long rmax = r[1];
+                long rmin = r[0];
+
+                max = Math.max(max, Math.max(val * rmax, val * rmin));
+                min = Math.min(min, Math.min(val * rmax, val * rmin));
+
+                dp[i][j] = new long[]{min, max};
             }
         }
 
-        long[] ans = f(0, 0, grid);
-        long max = ans[1];
+        long max = dp[0][0][1];
 
         if(max < 0) return -1;
         max %= mod;
         return (int) max;
-    }
-
-    public long[] f(int i, int j, int[][] grid){
-        int val = grid[i][j];
-        if(i == n-1 && j == m-1) return new long[]{val, val};
-
-        if(dp[i][j][0] != Long.MAX_VALUE && dp[i][j][1] != Long.MIN_VALUE) return dp[i][j];
-
-        long max = Long.MIN_VALUE;
-        long min = Long.MAX_VALUE;
-
-        if(i + 1 < n){
-            long[] down = f(i+1, j, grid);
-            long dmax = down[1];
-            long dmin = down[0];
-
-            max = Math.max(max, Math.max(val * dmax, val * dmin));
-            min = Math.min(min, Math.min(val * dmax, val * dmin));
-        }
-
-        if(j + 1 < m){
-            long[] r = f(i, j+1, grid);
-            long rmax = r[1];
-            long rmin = r[0];
-
-            max = Math.max(max, Math.max(val * rmax, val * rmin));
-            min = Math.min(min, Math.min(val * rmax, val * rmin));
-        }
-
-        long[] ans = new long[]{min, max};
-        dp[i][j] = ans;
-        return ans;
     }
 }
