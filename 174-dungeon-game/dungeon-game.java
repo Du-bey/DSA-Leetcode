8e6ebcf71352e392d42aaa0c392f1cb5a738715a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2025-09-06 02:59:13 +0530

    Time: 2 ms (67.76%) | Memory: 45.7 MB (17.47%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,29 +1,28 @@
 class Solution {
-    int n;
-    int m;
-    int[][] dp;
     public int calculateMinimumHP(int[][] d) {
-        n = d.length;
-        m = d[0].length;
-        dp = new int[n][m];
-        for(int[] r : dp){
-            Arrays.fill(r, -1);
+        int n = d.length;
+        int m = d[0].length;
+        int[][] dp = new int[n][m];
+
+        dp[n-1][m-1] = Math.max(1, 1 - d[n-1][m-1]);
+
+        for(int i = n-2;i>=0;i--){
+            dp[i][m-1] = Math.max(1, dp[i+1][m-1] - d[i][m-1]);
         }
-        return f(0, 0, d);
-    }
 
-    public int f(int i, int j, int[][] d){
-        if(i == n || j == m) return 1000000;
-        if(i == n - 1 && j == m - 1){
-            return Math.max(1, 1 - d[i][j]);
+        for(int j = m-2;j>=0;j--){
+            dp[n-1][j] = Math.max(1, dp[n-1][j+1] - d[n-1][j]);
         }
-        
-        if(dp[i][j] != -1) return dp[i][j];
 
-        int down = f(i+1, j, d);
-        int r = f(i, j+1, d);
+        for(int i = n-2;i>=0;i--){
+            for(int j = m-2;j>=0;j--){
+                int down = dp[i+1][j];
+                int r = dp[i][j+1];
 
-        int ans = Math.min(down, r) - d[i][j];
-        return dp[i][j] = Math.max(1, ans);
+                int ans = Math.min(down, r) - d[i][j];
+                dp[i][j] = Math.max(1, ans);
+            }
+        }
+        return dp[0][0];
     }
 }
