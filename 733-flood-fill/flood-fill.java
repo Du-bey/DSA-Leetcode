b56f8bf363b1f3b63fdd49d2e633559aa16c472b
Du-bey <54775278+Du-bey@users.noreply.github.com>
2026-01-31 19:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (86.84%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,33 +1,31 @@
 class Solution {
+    int[] dr = {0, 1, 0, -1};
+    int[] dc = {1, 0, -1, 0};
+
+    int n;
+    int m;
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int oc = image[sr][sc];
         if(oc == color) return image;
 
-        int n = image.length;
-        int m = image[0].length;
+        n = image.length;
+        m = image[0].length;    
 
-        int[] dr = {0, 1, 0, -1};
-        int[] dc = {1, 0, -1, 0};
-
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr, sc));
         image[sr][sc] = color;
+        dfs(sr, sc, color, oc, image);
+        return image;
+    }
 
-        while(!q.isEmpty()){
-            Pair p = q.poll();
-            int r = p.a;
-            int c = p.b;
-
-            for(int i = 0;i < 4;i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-                if(nr >= 0 && nc >=0 && nr < n && nc < m && image[nr][nc] == oc){
-                    q.add(new Pair(nr, nc));
-                    image[nr][nc] = color;
-                }
+    public void dfs(int r, int c, int color, int oc, int[][] image){
+        for(int i = 0;i < 4;i++){
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if(nr >= 0 && nc >=0 && nr < n && nc < m && image[nr][nc] == oc){
+                image[nr][nc] = color;
+                dfs(nr, nc, color, oc, image);
             }
         }
-        return image;
     }
 }
 
