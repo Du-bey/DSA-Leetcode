97e591cce2cf5a9caef913e6d7aad4c3b1ac3bfb
Du-bey <54775278+Du-bey@users.noreply.github.com>
2026-01-31 18:08:06 +0530

    Time: 5 ms (11.76%) | Memory: 48.7 MB (27.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,31 +1,45 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        boolean[] vis = new boolean[n];
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i =0;i<n;i++){
+        int n = isConnected.length;
+
+        for(int i = 0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n;j++){
-                if(isConnected[i][j] == 1) adj.get(i).add(j);
+
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j] == 1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
             }
         }
+
+        boolean[] vis = new boolean[n];
+
         int cnt = 0;
-        for(int i =0;i<n;i++){
+        for(int i = 0;i<n;i++){
             if(!vis[i]){
-                dfs(i, adj, vis);
                 cnt++;
+                bfs(i, vis, adj);
             }
         }
         return cnt;
     }
 
-    public void dfs(int u, List<List<Integer>> adj, boolean[] vis){
-        vis[u] = true;
-        for(int v : adj.get(u)){
-            if(!vis[v]){
-                dfs(v, adj, vis);
+    public void bfs(int u, boolean[] vis, List<List<Integer>> adj){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(u);
+        
+        while(!q.isEmpty()){
+            int node = q.poll();
+            vis[node] = true;
+            for(int v : adj.get(node)){
+                if(!vis[v]){
+                    q.add(v);
+                    vis[v] = true;
+                }
             }
         }
     }
