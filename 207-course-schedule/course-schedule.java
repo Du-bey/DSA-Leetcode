ddbeefc1fba1e13012898a58530a51cf6642f4ec
Du-bey <54775278+Du-bey@users.noreply.github.com>
2026-02-01 03:06:30 +0530

    Time: 7 ms (61.91%) | Memory: 47.2 MB (45.14%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,26 +1,28 @@
 class Solution {
     public boolean canFinish(int n, int[][] p) {
         List<List<Integer>> adj = new ArrayList<>();
+
         for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
         }
+
         int[] indegree = new int[n];
+        
         for(int[] r : p){
-            int u = r[0];
-            int v = r[1];
-            adj.get(v).add(u);
-            indegree[u]++;
+            adj.get(r[1]).add(r[0]);
+            indegree[r[0]]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
         for(int i =0;i<n;i++){
-            if(indegree[i] == 0){
-                q.add(i);
-            }
+            if(indegree[i] == 0) q.add(i);
         }
+        
         int cnt = 0;
         while(!q.isEmpty()){
             int u = q.poll();
             cnt++;
+
             for(int v : adj.get(u)){
                 indegree[v]--;
                 if(indegree[v] == 0){
