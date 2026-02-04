d5a95f0596299975bb11132264df2d55ef866d8a
Du-bey <54775278+Du-bey@users.noreply.github.com>
2026-02-04 21:04:35 +0530

    Time: 47 ms (38.26%) | Memory: 202.6 MB (51.11%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,5 +1,4 @@
 class Solution {
-    int ind = 0;
     int cnt;
     public long countPairs(int n, int[][] edges) {
         if(edges.length == 0){
@@ -9,7 +8,6 @@ class Solution {
         cnt = 0;
 
         List<List<Integer>> adj = new ArrayList<>();
-        Map<Integer, Integer> h = new HashMap<>();
 
         for(int i =0;i<n;i++){
             adj.add(new ArrayList<>());
@@ -24,25 +22,15 @@ class Solution {
             adj.get(v).add(u);
         }
 
+
+        long ans = 0;
         for(int i =0;i<n;i++){
             if(!vis[i]){
                 dfs(i, vis, adj);
-                h.put(ind, cnt);
+                long sum = 1L * cnt * (n - cnt);
+                ans += sum;
                 cnt = 0;
-                ind++;
             }
-        }
-
-        long ans = 0;
-        long sum = 0;
-        for(int k : h.keySet()){
-            int v1 = h.get(k);
-            sum += v1;
-        }
-
-        for(int v : h.values()){
-            long rem = sum - v;
-            ans += 1L * rem * v;
         }
 
         return ans / 2;
