71c13a56d89f4f75cb3c794e4de9d600979d883e
Du-bey <54775278+Du-bey@users.noreply.github.com>
2026-02-11 02:06:09 +0530

    Time: 320 ms (82.54%) | Memory: 47.8 MB (92.06%) - LeetSync

--- 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.java
+++ 3633-maximize-the-number-of-target-nodes-after-connecting-trees-i/maximize-the-number-of-target-nodes-after-connecting-trees-i.java
@@ -3,6 +3,25 @@ class Solution {
         int n = edges1.length + 1;
         int m = edges2.length + 1;
 
+        List<List<Integer>> adj = buildGraph(m, edges2);
+        List<List<Integer>> adj2 = buildGraph(n, edges1);
+
+        int maxd2 = 0;
+        for(int i = 0;i<m;i++){
+            maxd2 = Math.max(maxd2, dfs(i, adj, -1, k-1));
+        }
+
+        int[] d1 = new int[n];
+
+        for(int i = 0;i<n;i++){
+            d1[i] = maxd2 + dfs(i, adj2, -1, k);
+        }
+
+        return d1;
+    }
+
+
+    public List<List<Integer>> buildGraph(int m, int[][] edges2){
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i =0;i<m;i++){
@@ -15,55 +34,20 @@ class Solution {
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-
-        List<List<Integer>> adj2 = new ArrayList<>();
-
-        for(int i =0;i<n;i++){
-            adj2.add(new ArrayList<>());
-        }
-
-        for(int[] r : edges1){
-            int u = r[0];
-            int v = r[1];
-            adj2.get(u).add(v);
-            adj2.get(v).add(u);
-        }
-
-        int maxd2 = 0;
-        for(int i = 0;i<m;i++){
-            maxd2 = Math.max(maxd2, bfs(i, adj, k-1, m));
-        }
-
-        int[] d1 = new int[n];
-
-        for(int i = 0;i<n;i++){
-            d1[i] = maxd2 + bfs(i, adj2, k, n) - 2;
-        }
-
-        return d1;
+        return adj;
     }
 
-    public int bfs(int i, List<List<Integer>> adj, int k, int m){
-        Queue<Integer> q = new LinkedList<>();
-        int d = 1;
-        q.add(i);
-        boolean[] vis = new boolean[m];
-        vis[i] = true;
+    public int dfs(int u, List<List<Integer>> adj, int p, int k){
+        if(k == 0) return 1;
+        if(k < 0) return 0;
 
-        while(k >= 0 && !q.isEmpty()){
-            int size = q.size();
-            k--; 
-            for(int j =0;j<size;j++){
-                int u = q.poll();
-                d++;
-                for(int v : adj.get(u)){
-                    if(!vis[v]){
-                        q.add(v);
-                        vis[v] = true;
-                    }
-                }
+        int ans = 1;
+
+        for(int v : adj.get(u)){
+            if(v != p){
+                ans += dfs(v, adj, u, k-1);
             }
         }
-        return d;
+        return ans;
     }
 }
