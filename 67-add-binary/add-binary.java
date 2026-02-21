05e154a3281fc0f88c90334866df7ab4df4f344f
Du-bey <54775278+Du-bey@users.noreply.github.com>
2026-02-21 10:50:23 +0530

    Time: 1 ms (99.85%) | Memory: 43.3 MB (97.48%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,22 +1,25 @@
 class Solution {
     public String addBinary(String a, String b) {
-        String ans = "";
-        int car = 0;
         int n = a.length();
         int m = b.length();
+
         int i = n-1;
         int j = m-1;
+        int car = 0;
+        StringBuilder sb = new StringBuilder();
         while(i >= 0 || j >= 0){
-            int sum = car;
-            if(i >= 0) sum += a.charAt(i--) - '0';
-            if(j >= 0) sum += b.charAt(j--) - '0';
-            car = sum > 1 ? 1 : 0;
-            char c = (char)(sum % 2 + '0');
-            ans = c + ans;
+            if(i >= 0){
+                car += a.charAt(i--) - '0';
+            }
+            if(j >= 0){
+                car += b.charAt(j--) - '0';
+            }
+
+            sb.append((char)((car % 2) + '0'));
+            car /= 2;
         }
-        if(car == 1){
-            ans = "1" + ans;
-        }
-        return ans;
+        if(car > 0) sb.append('1');
+
+        return sb.reverse().toString();
     }
 }
